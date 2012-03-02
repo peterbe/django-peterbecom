af85f251ee2d7d58c0fa7581bf8b1f83dc8a4cd8
Peter Bengtsson <mail@peterbe.com>
2012-03-01 18:02:58 -0800

    adding django_compressor as vendor submodule

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import os, sys
+import site
+_here = os.path.dirname(__file__)
+site.addsitedir(os.path.join(_here, 'vendor'))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peterbecom.settings")
