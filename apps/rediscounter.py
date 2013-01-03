0bba272c5f40c33fc548206eae51d2c27dc29d14
Peter Bengtsson <mail@peterbe.com>
2013-01-03 12:16:50 -0800

    fix for urls ending with /

--- apps/rediscounter.py
+++ apps/rediscounter.py
@@ -4,5 +4,8 @@ from apps.redisutils import get_redis_connection
 
 def redis_increment(prefix, request):
     redis = get_redis_connection()
-    value = '%s %s' % (request.method, iri_to_uri(request.get_full_path()))
+    full_path = request.get_full_path()
+    if full_path != '/' and full_path.endswith('/'):
+        full_path = full_path[:-1]
+    value = '%s %s' % (request.method, iri_to_uri(full_path))
     redis.zincrby(prefix, value, 1)
