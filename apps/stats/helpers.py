4bb0eb37c3da6759e889eec3a795d6c76ff1e74a
Peter Bengtsson <mail@peterbe.com>
2013-01-24 13:46:57 -0800

    python 2.6 :(

--- apps/stats/helpers.py
+++ apps/stats/helpers.py
@@ -3,4 +3,11 @@ from jingo import register
 
 @register.function
 def thousands(v):
-    return format(v, ',')
+    # if only this was python 2.7!! :(
+    #return format(v, ',')
+    r = []
+    for i, c in enumerate(reversed(str(n))):
+        if i and (not (i % 3)):
+            r.insert(0, ',')
+        r.insert(0, c)
+    return ''.join(r)
