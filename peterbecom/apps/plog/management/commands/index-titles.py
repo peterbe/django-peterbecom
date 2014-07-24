c8b791e84839fe5651b0c2a372d5e612461ef9fc
Peter Bengtsson <mail@peterbe.com>
2014-07-23 21:22:42 -0700

    less verbose index-titles

--- peterbecom/apps/plog/management/commands/index-titles.py
+++ peterbecom/apps/plog/management/commands/index-titles.py
@@ -15,18 +15,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = utc_now()
+        verbose = int(options['verbosity']) > 1
 
         connection = get_redis_connection('titles')
         connection.flushdb()
         search_index = RedisSearchIndex(connection)
 
         for plog in models.BlogItem.objects.filter(pub_date__lte=now).order_by('?'):
-            # if 'Gro' in plog.title:
-            print plog.title,
+            if verbose:
+                print plog.title,
             # print search_index.add_item(plog.id, plog.title, 1)
             try:
                 hits = models.BlogItemHits.objects.get(oid=plog.oid).hits
             except models.BlogItemHits.DoesNotExist:
                 hits = 1
-            # if 'Gro' in plog.title:
-            print search_index.add_item(plog.oid, plog.title, hits), hits
+            if verbose:
+                print search_index.add_item(plog.oid, plog.title, hits), hits
