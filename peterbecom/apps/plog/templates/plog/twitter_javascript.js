38c70705708d09eb62ee4e4fcc8dd8fe79552fcf
Peter Bengtsson <mail@peterbe.com>
2014-11-05 21:41:22 -0800

    delay twitter widget

---
+++ peterbecom/apps/plog/templates/plog/twitter_javascript.js
@@ -0,0 +1,8 @@
+$(function() {
+  var twittercode = function(d,s,id, callback){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";js.onload=callback;fjs.parentNode.insertBefore(js,fjs);}};
+  setTimeout(function() {
+    twittercode(document,"script","twitter-wjs", function() {
+      document.getElementById('buttons').style.display = 'block';
+    });
+  }, 1000);
+});
