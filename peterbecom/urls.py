a9ae989273eee7f88f9765ec680b20c4f09f9e27
Peter Bengtsson <mail@peterbe.com>
2015-07-19 12:51:15 -0700

    comment out admin

--- peterbecom/urls.py
+++ peterbecom/urls.py
@@ -4,8 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
