09876453a1048e161b8ceb92f1d5d710ecbe1498
Peter Bengtsson <mail@peterbe.com>
2015-08-01 15:02:05 -0700

    rearrange things

--- bin/lint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-#flake8 airmozilla --exclude=*/migrations/*,airmozilla/settings/*.py,airmozilla/base/akamai_token_v2.py
--- bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# pwd is the git repo.
-set -e
-
-python manage.py test --no-input
