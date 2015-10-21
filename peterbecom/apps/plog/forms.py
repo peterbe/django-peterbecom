207e3bb06c890d9435a1da5872f0897729841070
Peter Bengtsson <mail@peterbe.com>
2015-10-21 09:46:05 -0700

    categories sorted by popularity

--- peterbecom/apps/plog/forms.py
+++ peterbecom/apps/plog/forms.py
@@ -3,7 +3,10 @@ import datetime
 from django import forms
 from django.forms.fields import ChoiceField
 from django.forms.widgets import Textarea
-from peterbecom.apps.plog.models import BlogItem, BlogFile
+from django.utils import timezone
+from django.db.models import Count
+
+from peterbecom.apps.plog.models import BlogItem, BlogFile, Category
 
 
 class MultilineTextarea(Textarea):
@@ -11,6 +14,7 @@ class MultilineTextarea(Textarea):
         if value:
             value = '\n'.join(value)
         return super(MultilineTextarea, self).render(name, value, attrs=attrs)
+
 
 class BlogForm(forms.ModelForm):
 
@@ -23,13 +27,49 @@ class BlogForm(forms.ModelForm):
         super(BlogForm, self).__init__(*args, **kwargs)
         self.fields['display_format'] = ChoiceField()
         self.fields['display_format'].choices = [
-          ('structuredtext', 'structuredtext'),
-          ('markdown', 'markdown'),
+            ('structuredtext', 'structuredtext'),
+            ('markdown', 'markdown'),
         ]
         self.fields['keywords'].widget = MultilineTextarea()
         self.fields['url'].required = False
         self.fields['summary'].required = False
         self.fields['keywords'].required = False
+
+        all_categories = dict(
+            Category.objects.all().values_list('id', 'name')
+        )
+        one_year = timezone.now() - datetime.timedelta(days=365)
+        qs = (
+            BlogItem.categories.through.objects
+            .filter(blogitem__pub_date__gte=one_year)
+            .values('category_id')
+            .annotate(Count('category_id'))
+            .order_by('-category_id__count')
+        )
+        category_choices = []
+        used = []
+        for count in qs:
+            pk = count['category_id']
+            combined = '{} ({})'.format(
+                all_categories[pk],
+                count['category_id__count']
+            )
+            category_choices.append(
+                (pk, combined)
+            )
+            used.append(pk)
+
+        category_items = all_categories.iteritems()
+        for pk, name in sorted(category_items, key=lambda x: x[1].lower()):
+            if pk in used:
+                continue
+            combined = '{} (0)'.format(
+                all_categories[pk],
+            )
+            category_choices.append(
+                (pk, combined)
+            )
+        self.fields['categories'].choices = category_choices
 
     def clean_oid(self):
         value = self.cleaned_data['oid']
