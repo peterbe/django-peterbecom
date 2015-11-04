b2411e8f164349149f26985ac9d6fbbf8c7b7543
Peter Bengtsson <mail@peterbe.com>
2015-11-04 10:50:18 -0800

    pingdom RUM code

--- peterbecom/apps/homepage/context_processors.py
+++ peterbecom/apps/homepage/context_processors.py
@@ -6,5 +6,6 @@ from django.conf import settings
 def context(request):
     return {
         'use_google_analytics': not settings.DEBUG,
+        'pingdom_rum_id': settings.PINGDOM_RUM_ID,
         'THIS_YEAR': datetime.datetime.utcnow().year,
     }
