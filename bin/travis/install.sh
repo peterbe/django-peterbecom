286010d779c337acf5c70e2bdf0d5b0e4eaa8de8
Peter Bengtsson <mail@peterbe.com>
2016-05-29 13:55:50 -0400

    latest pip in travis

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -2,6 +2,9 @@
 # pwd is the git repo.
 set -e
 
+echo "Install latestish pip"
+pip install "pip==8.1.2"
+
 # Before installation, we'll run ``pip wheel``, this will build wheels for
 # anything that doesn't already have one on PyPI.
 pip wheel -r requirements.txt
