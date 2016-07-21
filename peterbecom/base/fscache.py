8fac2aa4cfdb11de6a52c544cd70218e46e448b6
Peter Bengtsson <mail@peterbe.com>
2016-07-21 11:00:50 -0400

    debugging paths we skip

--- peterbecom/base/fscache.py
+++ peterbecom/base/fscache.py
@@ -10,6 +10,8 @@ file_extension_re = re.compile('\w+\.\w{2,4}$')
 
 
 def path_to_fs_path(path):
+    if not file_extension_re.findall(path):
+        print "SKIPPING", path
     if path.endswith('/') or not file_extension_re.findall(path):
         fs_path = settings.FSCACHE_ROOT
         for directory in path.split('/'):
