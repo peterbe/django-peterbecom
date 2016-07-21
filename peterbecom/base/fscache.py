d718e07a3a2f92c1dd8d230c2cd10c095f5e4d8c
Peter Bengtsson <mail@peterbe.com>
2016-07-21 11:02:10 -0400

    fix debugging on fileextension

--- peterbecom/base/fscache.py
+++ peterbecom/base/fscache.py
@@ -10,7 +10,7 @@ file_extension_re = re.compile('\w+\.\w{2,4}$')
 
 
 def path_to_fs_path(path):
-    if not file_extension_re.findall(path):
+    if file_extension_re.findall(path):
         print "SKIPPING", path
     if path.endswith('/') or not file_extension_re.findall(path):
         fs_path = settings.FSCACHE_ROOT
