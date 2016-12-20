e048708910b3081c4a851e962aa0237081a1c789
Peter Bengtsson <mail@peterbe.com>
2016-12-20 12:58:13 -0500

    exclude podcasts with errors

---
+++ peterbecom/podcasttime/migrations/0018_podcast_error.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('podcasttime', '0017_auto_20160322_0505'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='podcast',
+            name='error',
+            field=models.TextField(null=True),
+        ),
+    ]
