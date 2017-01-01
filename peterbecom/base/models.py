2c0f2795d2b63e74251ee8c32434f6c418508e39
Peter Bengtsson <mail@peterbe.com>
2016-12-31 19:28:33 -0500

    correct filter on podcasts stats

--- peterbecom/base/models.py
+++ peterbecom/base/models.py
@@ -4,8 +4,8 @@ from jsonfield import JSONField
 
 
 class CommandRun(models.Model):
-    command = models.CharField(max_length=100)
     app = models.CharField(max_length=100)
+    command = models.CharField(max_length=100)
     duration = models.DurationField()
     notes = models.TextField(null=True)
     exception = models.TextField(null=True)
@@ -13,8 +13,9 @@ class CommandRun(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self):
-        return '<{}: {!r}{}>'.format(
+        return '<{}: {!r} {!r}{}>'.format(
             self.__class__.__name__,
+            self.app,
             self.command,
             self.exception and ' (Errored)' or '',
         )
