99e0154a3ea6469b9811b46016bdd72b1c034855
Peter Bengtsson <peterbe@mozilla.com>
2017-03-28 12:19:19 -0400

    fix travis build (#83)
    * fix travis build
    
    * up wheel too
    
    * don't pip wheel first
    
    * latest packaging
    
    * adding pyparsing
    
    * cleaning up
    
    * fix bad test

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -3,11 +3,7 @@
 set -e
 
 echo "Install latestish pip"
-pip install "pip==8.1.2"
-
-# Before installation, we'll run ``pip wheel``, this will build wheels for
-# anything that doesn't already have one on PyPI.
-pip wheel -r requirements.txt
+pip install -U pip wheel
 
 echo "Install Python dependencies"
 pip install -r requirements.txt
