04db71f391c0fe61bebae42fd678903c514dc685
Peter Bengtsson <mail@peterbe.com>
2017-03-28 11:53:35 -0400

    don't pip wheel first

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -5,9 +5,12 @@ set -e
 echo "Install latestish pip"
 pip install -U pip wheel
 
+echo "What pip version did we get?"
+pip --version
+
 # Before installation, we'll run ``pip wheel``, this will build wheels for
 # anything that doesn't already have one on PyPI.
-pip wheel -r requirements.txt
+# pip wheel -r requirements.txt
 
 echo "Install Python dependencies"
 pip install -r requirements.txt
