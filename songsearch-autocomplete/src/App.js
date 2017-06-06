a9eb3d3297d682932587f0684d972ec27e4c692c
Peter Bengtsson <mail@peterbe.com>
2017-06-06 19:15:03 -0400

    adding songsearch-complete and upgrading requests

---
+++ songsearch-autocomplete/src/App.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+const appendSuggestion = (text, append) => {
+  let split = text.split(/\s+/)
+  split.pop()
+  split.push(append)
+  return split.join(' ')
+}
+
+
+class App extends Component {
+
+  state = {
+    q: '',
+    searching: false,
+    // searchMaxLength: null,
+    autocompleteSuggestions: null,
+    autocompleteHighlight: -1,
+    showAutocompleteSuggestions: true,
+  }
+
+  submitSearch = (event) => {
+    event.preventDefault();
+    this._submit(this.state.q)
+  }
+
+  _submit = (q) => {
+    console.warn("GOTO!!!", 'https://songsear.ch/q/' + q);
+    // document.location.href = 'https://songsear.ch/q/' + q;
+    if (this.state.autocompleteSuggestions) {
+      this.setState({
+        autocompleteSuggestions: null,
+        autocompleteHighlight: -1,
+        showAutocompleteSuggestions: true,
+      })
+    }
+  }
+
+  onFocusSearch = (event) => {
+    if (!this.state.showAutocompleteSuggestions) {
+        this.setState({showAutocompleteSuggestions: true})
+    }
+  }
+
+  onBlurSearch = (event) => {
+    setTimeout(() => {
+      this.setState({
+        showAutocompleteSuggestions: false,
+      })
+    }, 300)
+  }
+
+  onChangeSearch = (event) => {
+    this.setState({q: event.target.value})
+    let length = event.target.value.length
+    // if (length > this.refs.q.maxLength - 10) {
+    //   this.setState({searchMaxLength: [
+    //     length,
+    //     this.refs.q.maxLength
+    //   ]})
+    // } else if (this.state.searchMaxLength) {
+    //   this.setState({searchMaxLength: null})
+    // }
+    if (length) {
+      this._fetchAutocompleteSuggestions(event.target.value.trim())
+    } else {
+        this.setState({
+          autocompleteSuggestions: null,
+          autocompleteHighlight: -1,
+          showAutocompleteSuggestions: true,
+        })
+    }
+  }
+
+  _fetchAutocompleteSuggestions = (q) => {
+    let url = `https://songsear.ch/api/search/autocomplete?q=${q}`
+    fetch(url)
+    .then(r => {
+      if (r.status === 200) {
+        r.json()
+        .then(results => {
+          this.setState({
+            autocompleteSuggestions: results.matches,
+            autocompleteHighlight: -1,
+          })
+        })
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  onKeyDownSearch = (event) => {
+    // console.log('keydown search');
+    let suggestions = this.state.autocompleteSuggestions
+    if (suggestions) {
+      let highlight = this.state.autocompleteHighlight
+      if (event.key === 'Tab') {
+        event.preventDefault();
+        let suggestion = highlight > -1 ? suggestions[highlight] : suggestions[0]
+        if (suggestion.append) {
+          this.setState({
+            q: appendSuggestion(this.state.q, suggestion.text) + ' ',
+            autocompleteSuggestions: null,
+            autocompleteHighlight: -1,
+          })
+        } else {
+          this.setState({
+            q: suggestion.text + ' ',
+          })
+          this._fetchAutocompleteSuggestions(
+            suggestion.text,
+          )
+        }
+      } else if (event.key === 'ArrowDown' && highlight < suggestions.length) {
+        event.preventDefault();
+        this.setState({autocompleteHighlight: highlight + 1})
+      } else if (event.key === 'ArrowUp' && highlight > -1) {
+        this.setState({autocompleteHighlight: highlight - 1})
+      } else if (event.key === 'Enter') {
+        if (highlight > -1) {
+          event.preventDefault();
+          this.setState({
+            q: suggestions[highlight].text,
+            autocompleteSuggestions: null,
+            autocompleteHighlight: -1,
+          }, () => this._submit(suggestions[highlight].text))
+        }
+      }
+    }
+  }
+
+  onSelectSuggestion = (event, suggestion) => {
+    event.preventDefault()
+    this._submit(suggestion.text)
+  }
+
+  render() {
+    return (
+      <form
+        style={{margin:'50px 0'}}
+        action="https://songsear.ch/q/"
+        onSubmit={this.submitSearch}>
+        <div className="ui big icon input fluid" style={{display: 'relative'}}>
+          <input
+            type="search"
+            name="term"
+
+            value={this.state.q}
+            onFocus={this.onFocusSearch}
+            onBlur={this.onBlurSearch}
+            onChange={this.onChangeSearch}
+            onKeyDown={this.onKeyDownSearch}
+
+            className="form-control x-large"
+            placeholder="Type your search here..."/>
+          <i className="search icon"></i>
+          {
+            this.state.autocompleteSuggestions && this.state.showAutocompleteSuggestions ?
+            <ShowAutocompleteSuggestions
+              onSelectSuggestion={this.onSelectSuggestion}
+              highlight={this.state.autocompleteHighlight}
+              suggestions={this.state.autocompleteSuggestions}
+            /> : null
+          }
+        </div>
+      </form>
+    );
+  }
+}
+
+export default App;
+
+
+const ShowAutocompleteSuggestions = ({
+  highlight,
+  suggestions,
+  onSelectSuggestion,
+}) => {
+  if (!suggestions.length) {
+    return null
+  }
+  return (
+    <div className="autocomplete">
+      <ul>
+        {
+          suggestions.map((s, index) => {
+            let className = index === highlight ? 'active' : ''
+            return (
+              <li key={index} className={className}
+                onClick={e => onSelectSuggestion(e, s)}>
+                <p dangerouslySetInnerHTML={{__html: s.html}}></p>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
