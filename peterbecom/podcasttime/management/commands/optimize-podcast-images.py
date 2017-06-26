882bbacd5ccd7dfde158de85639002a4da712785
Peter Bengtsson <mail@peterbe.com>
2017-06-26 10:57:58 -0400

    louder optimizations

--- peterbecom/podcasttime/management/commands/optimize-podcast-images.py
+++ peterbecom/podcasttime/management/commands/optimize-podcast-images.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 from PIL import Image
 
@@ -10,26 +11,40 @@ from peterbecom.podcasttime.models import Podcast
 
 class Command(BaseCommand):
 
-    def _handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument('--limit', default=1000)
+
+    def _handle(self, **options):
+        limit = int(options['limit'])
         qs = Podcast.objects.filter(image__isnull=False)
+        iterator = qs.order_by('?')[:limit]
+        with tempfile.TemporaryDirectory() as tmp_directory:
+            self._process(tmp_directory, iterator)
+
+    def _process(self, tmp_directory, iterator):
         savings = []
         skips = 0
-        for podcast in qs.order_by('?')[:1000]:
+        for podcast in iterator:
             try:
                 path = podcast.image.path
+                self.warning("{!r} doesn't have an image ({!r})".format(
+                    podcast,
+                    podcast.image,
+                ))
             except ValueError:
                 continue
             if not os.path.isfile(path):
-                print("Not a file", path)
+                self.warning("Not a file", path)
                 continue
             log_file = path + '.optimized'
             if os.path.isfile(log_file):
+                print(log_file)
                 skips += 1
                 continue
             try:
                 img = Image.open(path)
             except OSError:
-                print("Completely broken image", path)
+                self.warning("Completely broken image", path)
                 os.remove(path)
                 podcast.image = None
                 podcast.save()
@@ -40,7 +55,7 @@ class Command(BaseCommand):
                 if not ext:
                     continue
                 if ext not in ('.jpg', '.jpeg', '.png'):
-                    print('Unrecognized extension {!r}'.format(ext))
+                    self.notice('Unrecognized extension {!r}'.format(ext))
                     continue
 
                 w2 = 1300
@@ -53,9 +68,21 @@ class Command(BaseCommand):
                 if ext in ('.jpg', '.jpeg'):
                     options['progressive'] = True
                 size_before = os.stat(path).st_size
-                print(path)
-                img.save(path, **options)
-                size_after = os.stat(path).st_size
+                tmp_path = os.path.join(tmp_directory, os.path.basename(path))
+                self.out('Saving {}'.format(tmp_path))
+                img.save(tmp_path, **options)
+                size_after = os.stat(tmp_path).st_size
+                if size_after > size_before:
+                    self.notice(
+                        '{} became larger after save'.format(
+                            path,
+                        )
+                    )
+
+                else:
+                    # Swap the old one for the new one
+                    os.rename(tmp_path, path)
+
                 with open(log_file, 'w') as f:
                     f.write(
                         'From {}, ({} bytes) to {} ({} bytes)\n'.format(
@@ -66,6 +93,8 @@ class Command(BaseCommand):
                         )
                     )
                 savings.append(size_before - size_after)
+            else:
+                self.out('Image too little ({}x{})'.format(w, h))
 
         if savings:
             self.out("SUM savings:", filesizeformat(sum(savings)))
