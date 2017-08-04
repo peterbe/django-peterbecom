8784443d33fca17d58c9834b2c7f8c9e5bb87da4
Peter Bengtsson <mail@peterbe.com>
2017-08-04 11:38:24 -0400

    prettier

--- songsearch-autocomplete/src/App.js
+++ songsearch-autocomplete/src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import { throttle } from 'throttle-debounce';
-
+import React, { Component } from 'react'
+import './App.css'
+import { throttle } from 'throttle-debounce'
 
 const appendSuggestion = (text, append) => {
   let split = text.split(/\s+/)
@@ -10,16 +9,14 @@ const appendSuggestion = (text, append) => {
   return split.join(' ')
 }
 
-
 class App extends Component {
-
   state = {
     q: '',
     searching: false,
     // searchMaxLength: null,
     autocompleteSuggestions: null,
     autocompleteHighlight: -1,
-    showAutocompleteSuggestions: true,
+    showAutocompleteSuggestions: true
   }
 
   componentDidMount() {
@@ -27,48 +24,48 @@ class App extends Component {
     // app started, that would be passed to the App when initialized.
     // For example if the React app is slow to load. I.e. async slow.
     if (this.props.initialValue && !this.state.q) {
-      this.setState({q: this.props.initialValue})
+      this.setState({ q: this.props.initialValue })
     }
   }
 
-  submitSearch = (event) => {
-    event.preventDefault();
+  submitSearch = event => {
+    event.preventDefault()
     this._submit(this.state.q)
   }
 
-  _submit = (q, submitevent='enter') => {
+  _submit = (q, submitevent = 'enter') => {
     // console.warn("GOTO!!!", 'https://songsear.ch/q/' + q);
     q = q.trim()
     if (!q) {
       return
     }
-    let gotoURL = `https://songsear.ch/q/${q}?autocomplete=${submitevent}`;
+    let gotoURL = `https://songsear.ch/q/${q}?autocomplete=${submitevent}`
     document.location.href = gotoURL
     if (this.state.autocompleteSuggestions) {
       this.setState({
         autocompleteSuggestions: null,
         autocompleteHighlight: -1,
-        showAutocompleteSuggestions: true,
+        showAutocompleteSuggestions: true
       })
     }
   }
 
-  onFocusSearch = (event) => {
+  onFocusSearch = event => {
     if (!this.state.showAutocompleteSuggestions) {
-        this.setState({showAutocompleteSuggestions: true})
+      this.setState({ showAutocompleteSuggestions: true })
     }
   }
 
-  onBlurSearch = (event) => {
+  onBlurSearch = event => {
     setTimeout(() => {
       this.setState({
-        showAutocompleteSuggestions: false,
+        showAutocompleteSuggestions: false
       })
     }, 300)
   }
 
-  onChangeSearch = (event) => {
-    this.setState({q: event.target.value})
+  onChangeSearch = event => {
+    this.setState({ q: event.target.value })
     let length = event.target.value.length
     // if (length > this.refs.q.maxLength - 10) {
     //   this.setState({searchMaxLength: [
@@ -79,34 +76,34 @@ class App extends Component {
     //   this.setState({searchMaxLength: null})
     // }
     if (length > 12) {
-      this._fetchAutocompleteSuggestionsThrottledLonger(event.target.value.trim())
+      this._fetchAutocompleteSuggestionsThrottledLonger(
+        event.target.value.trim()
+      )
     } else if (length > 5) {
       this._fetchAutocompleteSuggestionsThrottled(event.target.value.trim())
     } else if (length) {
       this._fetchAutocompleteSuggestions(event.target.value.trim())
     } else {
-        this.setState({
-          autocompleteSuggestions: null,
-          autocompleteHighlight: -1,
-          showAutocompleteSuggestions: true,
-        })
+      this.setState({
+        autocompleteSuggestions: null,
+        autocompleteHighlight: -1,
+        showAutocompleteSuggestions: true
+      })
     }
   }
 
-   _last_fetch_term = null
+  _last_fetch_term = null
 
-  _fetchAutocompleteSuggestions = (q) => {
+  _fetchAutocompleteSuggestions = q => {
     let url = `https://songsear.ch/api/search/autocomplete?q=${q}`
     this._last_fetch_term = q
-    fetch(url)
-    .then(r => {
+    fetch(url).then(r => {
       if (r.status === 200) {
         if (q === this._last_fetch_term) {
-          r.json()
-          .then(results => {
+          r.json().then(results => {
             this.setState({
               autocompleteSuggestions: results.matches,
-              autocompleteHighlight: -1,
+              autocompleteHighlight: -1
             })
           })
         }
@@ -115,49 +112,53 @@ class App extends Component {
   }
 
   _fetchAutocompleteSuggestionsThrottled = throttle(
-    500, this._fetchAutocompleteSuggestions
+    500,
+    this._fetchAutocompleteSuggestions
   )
 
   _fetchAutocompleteSuggestionsThrottledLonger = throttle(
-    1500, this._fetchAutocompleteSuggestions
+    1500,
+    this._fetchAutocompleteSuggestions
   )
 
-  onKeyDownSearch = (event) => {
+  onKeyDownSearch = event => {
     let suggestions = this.state.autocompleteSuggestions
     if (suggestions) {
       let highlight = this.state.autocompleteHighlight
       if (event.key === 'Tab') {
-        event.preventDefault();
-        let suggestion = highlight > -1 ? suggestions[highlight] : suggestions[0]
+        event.preventDefault()
+        let suggestion =
+          highlight > -1 ? suggestions[highlight] : suggestions[0]
         if (suggestion.append) {
           this.setState({
             q: appendSuggestion(this.state.q, suggestion.text) + ' ',
             autocompleteSuggestions: null,
-            autocompleteHighlight: -1,
+            autocompleteHighlight: -1
           })
         } else {
           this.setState({
-            q: suggestion.text + ' ',
+            q: suggestion.text + ' '
           })
-          this._fetchAutocompleteSuggestions(
-            suggestion.text,
-          )
+          this._fetchAutocompleteSuggestions(suggestion.text)
         }
       } else if (event.key === 'ArrowDown' && highlight < suggestions.length) {
-        event.preventDefault();
-        this.setState({autocompleteHighlight: highlight + 1})
+        event.preventDefault()
+        this.setState({ autocompleteHighlight: highlight + 1 })
       } else if (event.key === 'ArrowUp' && highlight > -1) {
-        this.setState({autocompleteHighlight: highlight - 1})
+        this.setState({ autocompleteHighlight: highlight - 1 })
       } else if (event.key === 'Enter') {
         if (highlight > -1) {
-          event.preventDefault();
-          this.setState({
-            q: suggestions[highlight].text,
-            autocompleteSuggestions: null,
-            autocompleteHighlight: -1,
-          }, () => {
-            this._submit(suggestions[highlight].text, 'enterkey')
-          })
+          event.preventDefault()
+          this.setState(
+            {
+              q: suggestions[highlight].text,
+              autocompleteSuggestions: null,
+              autocompleteHighlight: -1
+            },
+            () => {
+              this._submit(suggestions[highlight].text, 'enterkey')
+            }
+          )
         }
       }
     }
@@ -171,44 +172,46 @@ class App extends Component {
   render() {
     return (
       <form
-        style={{margin:'50px 0'}}
+        style={{ margin: '50px 0' }}
         action="https://songsear.ch/q/"
-        onSubmit={this.submitSearch}>
-        <div className="ui big icon input fluid" style={{display: 'relative'}}>
+        onSubmit={this.submitSearch}
+      >
+        <div
+          className="ui big icon input fluid"
+          style={{ display: 'relative' }}
+        >
           <input
             type="search"
             name="term"
-
             value={this.state.q}
             onFocus={this.onFocusSearch}
             onBlur={this.onBlurSearch}
             onChange={this.onChangeSearch}
             onKeyDown={this.onKeyDownSearch}
-
             className="form-control x-large"
-            placeholder="Type your search here..."/>
-          <i className="search icon"></i>
-          {
-            this.state.autocompleteSuggestions && this.state.showAutocompleteSuggestions ?
-            <ShowAutocompleteSuggestions
-              onSelectSuggestion={this.onSelectSuggestion}
-              highlight={this.state.autocompleteHighlight}
-              suggestions={this.state.autocompleteSuggestions}
-            /> : null
-          }
+            placeholder="Type your search here..."
+          />
+          <i className="search icon" />
+          {this.state.autocompleteSuggestions &&
+          this.state.showAutocompleteSuggestions
+            ? <ShowAutocompleteSuggestions
+                onSelectSuggestion={this.onSelectSuggestion}
+                highlight={this.state.autocompleteHighlight}
+                suggestions={this.state.autocompleteSuggestions}
+              />
+            : null}
         </div>
       </form>
-    );
+    )
   }
 }
 
-export default App;
-
+export default App
 
 const ShowAutocompleteSuggestions = ({
   highlight,
   suggestions,
-  onSelectSuggestion,
+  onSelectSuggestion
 }) => {
   if (!suggestions.length) {
     return null
@@ -216,17 +219,18 @@ const ShowAutocompleteSuggestions = ({
   return (
     <div className="autocomplete">
       <ul>
-        {
-          suggestions.map((s, index) => {
-            let className = index === highlight ? 'active' : ''
-            return (
-              <li key={index} className={className}
-                onClick={e => onSelectSuggestion(e, s)}>
-                <p dangerouslySetInnerHTML={{__html: s.html}}></p>
-              </li>
-            )
-          })
-        }
+        {suggestions.map((s, index) => {
+          let className = index === highlight ? 'active' : ''
+          return (
+            <li
+              key={index}
+              className={className}
+              onClick={e => onSelectSuggestion(e, s)}
+            >
+              <p dangerouslySetInnerHTML={{ __html: s.html }} />
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
