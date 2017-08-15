df58ac84e2a9a1c006759edff82c51373d8b2e19
Peter Bengtsson <mail@peterbe.com>
2017-08-15 08:17:48 -0400

    avoid too long http_referer

--- peterbecom/plog/tasks.py
+++ peterbecom/plog/tasks.py
@@ -20,6 +20,8 @@ def increment_blogitem_hit(
     remote_addr=None,
     http_referer=None,
 ):
+    if http_referer and len(http_referer) > 450:
+        http_referer = http_referer[:450 - 3] + '...'
     try:
         BlogItemHit.objects.create(
             blogitem=BlogItem.objects.get(oid=oid),
