9a67dff9dd93203a94f93ee55797b5d44b49c87d
Peter Bengtsson <mail@peterbe.com>
2018-01-12 09:48:33 -0500

    better prefetcher

--- peterbecom/base/static/js/prefetcher.js
+++ peterbecom/base/static/js/prefetcher.js
@@ -2,32 +2,56 @@
 $(function() {
   'use strict';
 
+  function preFetchImage(url, callback) {
+    var img = new Image();
+    img.onload = function() {
+      callback(url);
+    };
+    img.src = url;
+  }
+
   var prefetched = [];
   var prefetchTimer = null;
-  $('.container').on('mouseover', 'a', function(e) {
-    if (!e.target.attributes.href) {
-      return;
-    }
-    if (e.target.attributes.rel && e.target.attributes.rel.value === 'nofollow') {
-      return;
-    }
-    var value = e.target.attributes.href.value;
-    if (value.indexOf('/') === 0) {
-      if (prefetched.indexOf(value) === -1) {
-        if (prefetchTimer) {
-          clearTimeout(prefetchTimer);
-        }
-        prefetchTimer = setTimeout(function() {
-          $.get(value, function() {
-            // necessary for $.ajax to start the request :(
-          });
-          prefetched.push(value);
-        }, 200);
+  $('.container')
+    .on('mouseover', 'a', function(e) {
+      if (!e.target.attributes.href) {
+        return;
       }
-    }
-  }).on('mouseout', 'a', function() {
-    if (prefetchTimer) {
-      clearTimeout(prefetchTimer);
-    }
-  });
+      if (
+        e.target.attributes.rel &&
+        e.target.attributes.rel.value === 'nofollow'
+      ) {
+        return;
+      }
+      var value = e.target.attributes.href.value;
+      if (value.indexOf('/') === 0) {
+        if (prefetched.indexOf(value) === -1) {
+          if (prefetchTimer) {
+            clearTimeout(prefetchTimer);
+          }
+          prefetchTimer = setTimeout(function() {
+            $.get(value).done(function(response) {
+              var imageRegex = /<img.*?src="([^">]*\/([^">]*?))".*?>/g;
+              var match;
+              while ((match = imageRegex.exec(response))) {
+                var url = match[1];
+                if (prefetched.indexOf(url) === -1 && prefetched.length < 5) {
+                  if (url.indexOf('cdn-2916.kxcdn.com') > -1) {
+                    preFetchImage(url, function(url) {
+                      prefetched.push(url);
+                    });
+                  }
+                }
+              }
+            });
+            prefetched.push(value);
+          }, 200);
+        }
+      }
+    })
+    .on('mouseout', 'a', function() {
+      if (prefetchTimer) {
+        clearTimeout(prefetchTimer);
+      }
+    });
 });
