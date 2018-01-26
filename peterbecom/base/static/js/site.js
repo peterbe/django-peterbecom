1eb66d2a5e98bcc3d04fd9eaf1a0d674fe941daa
Peter Bengtsson <mail@peterbe.com>
2018-01-26 15:02:56 -0500

    lazy loading images

--- peterbecom/base/static/js/site.js
+++ peterbecom/base/static/js/site.js
@@ -2,78 +2,23 @@
 $(function() {
   'use strict';
 
-  // fix main menu to page on passing
-  // $('.main.menu').visibility({
-  //   type: 'fixed',
-  // });
-
   // show dropdown on hover
   $('.main.menu .ui.dropdown').dropdown({
     on: 'hover',
   });
 
-  // $('a.page-title-repeated').on('click', function() {
-  //   window.scrollTo(0, 0);
-  //   return false;
-  // });
+  function replaceLazyImages() {
+    $('div.post img[data-originalsrc]').each(function() {
+      var $img = $(this);
+      $img.attr('src', $img.data('originalsrc'))
+      $img.removeData('originalsrc');
 
-  // First decide which blog posts to "collapse"
-  $('div.post').each(function(i) {
-    if (i < 3 && document.location.search.search(/page=/) === -1) {
-      // First of all, don't bother with the first 3 that are likely
-      // to be within reach of the first page scrolling.
-      // However, this rule only applies if you're on page 1 of the
-      // home page.
-      return;
-    }
-    var element = $(this);
-    var height = element.height();
-    if (height < 600) { // XXX is this a good number?
-      // don't bother with the small ones
-      return;
-    }
-    element.data('height', height);
-    element.addClass('snippet');
-    $('<p>')
-      .addClass('read-more')
-      .append(
-        $('<a>')
-          .attr('href', $('h2 a', element).attr('href'))
-          .addClass('ui').addClass('button').addClass('primary')
-          .text('Read the rest of the blog post')
-      )
-      .appendTo(element);
-  });
+    })
+  }
 
-  // copied and chopped from https://css-tricks.com/text-fade-read-more/
-  $('div.post.snippet .read-more a.button').on('click', function(event) {
-    event.preventDefault();
-    var $el = $(this);
-    var $post = $el.parents('div.post.snippet');
-    var $readmore = $el.parent();
-
-    var currentHeight = $post.height();
-    var originalHeight = $post.data('height');
-
-    $post
-      .css({
-        // Set height to prevent instant jumpdown when max height is removed
-        'height': currentHeight,
-        'max-height': 9999,
-      })
-      .animate({
-        // Add a little 16px margin since that's excluded in the height
-        // but something the last <p> tag's margin normally adds
-        'height': originalHeight + 16,
-      });
-
-    // fade out read-more
-    $readmore.fadeOut(400, function() {
-      // now we can remove height stuff and the snippet class
-      $post
-        .removeClass('snippet');
-    });
-
+  $(window).on('scroll', function(event) {
+    $(window).off('scroll');
+    replaceLazyImages();
   });
 
 });
--- peterbecom/homepage/templatetags/jinja_helpers.py
+++ peterbecom/homepage/templatetags/jinja_helpers.py
@@ -1,7 +1,12 @@
+import re
+
 from django.core.urlresolvers import reverse
 from django.template import defaultfilters
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django_jinja import library
+
+image_tags_regex = re.compile('(<img src="([^"]+)"[^>]+>)')
+width_or_height_regex = re.compile('(width|height)="\d+"')
 
 
 @library.global_function
@@ -18,3 +23,27 @@ def static(path):
 @library.global_function
 def floatformat(*args, **kwargs):
     return defaultfilters.floatformat(*args, **kwargs)
+
+
+@library.global_function
+def make_images_lazy(html, placeholder_image_url):
+
+    def replacer(match):
+        html_tag, old_url = match.groups()
+        # Only do this trick for images that have a width and height set.
+        # Otherwise, when we replace our tiny placeholder image, it'll
+        # appear as tiny as the placeholder PNG is. But when eventually
+        # replaced, it'll appear as big as the original image.
+        # That replacement might cause the page height to change
+        # unnecessarily.
+        if width_or_height_regex.findall(html_tag):
+            new_snippet = '{}" data-originalsrc="{}'.format(
+                placeholder_image_url,
+                old_url,
+            )
+            html_tag = html_tag.replace(old_url, new_snippet)
+        else:
+            html_tag = match.group()
+        return html_tag
+
+    return image_tags_regex.sub(replacer, html)
