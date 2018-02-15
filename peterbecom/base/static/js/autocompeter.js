82de6027e9fccc5555556a49650df86b7994579f
Peter Bengtsson <mail@peterbe.com>
2018-02-14 20:16:20 -0500

    delayed loading of autocompeter.min.js

--- peterbecom/base/static/js/autocompeter.js
+++ peterbecom/base/static/js/autocompeter.js
@@ -1,16 +1,31 @@
 'use strict';
 
-// Idea, only load this IF there's focus on the input[name="q"] at all.
-
 (function() {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/autocompeter/1/autocompeter.min.js';
-  script.onload = function() {
-    window.Autocompeter(document.querySelectorAll('[name="q"]')[0], {
-      url: '/autocompete/v1',
-      domain: document.location.host,
-      ping: false,
-    });
-  };
-  document.head.appendChild(script);
+  function injectAutocompeterJS() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/autocompeter/1/autocompeter.min.js';
+    script.onload = function() {
+      window.Autocompeter(inputElement, {
+        url: '/autocompete/v1',
+        domain: document.location.host,
+        ping: false,
+      });
+    };
+    document.head.appendChild(script);
+  }
+
+  var injectedJS = false;
+  function onFocusInput(event) {
+    if (!injectedJS) {
+      injectedJS = true;
+      inputElement.removeEventListener('mouseover', onFocusInput, false);
+      injectAutocompeterJS();
+    }
+    return true;
+  }
+
+  var inputElement = document.querySelector('[name="q"]');
+  if (inputElement) {
+    inputElement.addEventListener('mouseover', onFocusInput, false);
+  }
 })();
