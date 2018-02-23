3ac4cefcbda9c17780be436ab96bc8224d8bfc34
Peter Bengtsson <mail@peterbe.com>
2018-02-23 13:25:53 -0500

    autocomplete

--- songsearch-autocomplete/src/App.js
+++ songsearch-autocomplete/src/App.js
@@ -1,197 +1,279 @@
-import React, { Component } from 'react'
-import './App.css'
-import { throttle } from 'throttle-debounce'
+import React from 'react';
+import './App.css';
+import { throttle, debounce } from 'throttle-debounce';
+import placeholderImage from './placeholder.png';
+
+const SERVER = process.env.REACT_APP_SERVER_URL || 'https://songsear.ch';
 
 const appendSuggestion = (text, append) => {
-  let split = text.split(/\s+/)
-  split.pop()
-  split.push(append)
-  return split.join(' ')
-}
+  let split = text.split(/\s+/);
+  split.pop();
+  split.push(append);
+  return split.join(' ');
+};
 
-class App extends Component {
-  state = {
-    q: '',
-    searching: false,
-    // searchMaxLength: null,
-    autocompleteSuggestions: null,
-    autocompleteHighlight: -1,
-    showAutocompleteSuggestions: true
+const absolutifyUrl = uri => {
+  if (uri.charAt(0) === '/' && uri.charAt(1) !== '/') {
+    return SERVER + uri;
   }
+  return uri;
+};
 
-  _fetchAutocompleteSuggestionsCache = {}
+const convertLazyLoadImages = (once = false) => {
+  const inner = () => {
+    const nodelist = document.querySelectorAll('img[data-src]');
+    let count = 0;
+    [...nodelist].forEach(img => {
+      const trueSrc = img.dataset.src;
+      delete img.dataset.src;
+      img.src = trueSrc;
+      if (img.classList) {
+        img.classList.remove('lazyload');
+        img.classList.remove('preview');
+      }
+      count++;
+    });
+    return count;
+  };
+  const wrap = debounce(200, true, inner);
+
+  if (once) {
+    wrap();
+  } else {
+    setTimeout(wrap, 40);
+    setTimeout(wrap, 300);
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: '',
+      // searching: false,
+      // searchMaxLength: null,
+      autocompleteSuggestions: null,
+      autocompleteHighlight: -1,
+      showAutocompleteSuggestions: true,
+    };
+
+    this.fetchAutocompleteSuggestionsDebounced = debounce(
+      400,
+      this.fetchAutocompleteSuggestions
+    );
+    this.fetchAutocompleteSuggestionsThrottled = throttle(
+      1500,
+      this.fetchAutocompleteSuggestions
+    );
+
+    this._fetchAutocompleteSuggestionsCache = {};
+  }
 
   componentDidMount() {
     // If the <input> HTML had something typed into it when the React
     // app started, that would be passed to the App when initialized.
     // For example if the React app is slow to load. I.e. async slow.
     if (this.props.initialValue && !this.state.q) {
-      this.setState({ q: this.props.initialValue })
+      this.setState({ q: this.props.initialValue });
     }
   }
 
   submitSearch = event => {
-    event.preventDefault()
-    this._submit(this.state.q)
-  }
+    event.preventDefault();
+    this._submit(this.state.q);
+  };
 
   _submit = (q, submitevent = 'enter') => {
-    // console.warn("GOTO!!!", 'https://songsear.ch/q/' + q);
-    // return;
-    q = q.trim()
+    q = q.trim();
     if (!q) {
-      return
+      return;
     }
-    let gotoURL = `https://songsear.ch/q/${q}?autocomplete=${submitevent}`
-    document.location.href = gotoURL
+    let gotoURL = `${SERVER}/q/${q}?autocomplete=${submitevent}`;
+    document.location.href = gotoURL;
     if (this.state.autocompleteSuggestions) {
       this.setState({
         autocompleteSuggestions: null,
         autocompleteHighlight: -1,
-        showAutocompleteSuggestions: true
-      })
+        showAutocompleteSuggestions: true,
+      });
     }
-  }
+  };
 
   onFocusSearch = event => {
     if (!this.state.showAutocompleteSuggestions) {
-      this.setState({ showAutocompleteSuggestions: true })
+      this.setState({ showAutocompleteSuggestions: true });
     }
-  }
+  };
 
   onBlurSearch = event => {
     setTimeout(() => {
       this.setState({
-        showAutocompleteSuggestions: false
-      })
-    }, 300)
-  }
+        showAutocompleteSuggestions: false,
+      });
+    }, 300);
+  };
 
   onChangeSearch = event => {
-    this.setState({ q: event.target.value })
-    let length = event.target.value.length
-    // if (length > this.refs.q.maxLength - 10) {
-    //   this.setState({searchMaxLength: [
-    //     length,
-    //     this.refs.q.maxLength
-    //   ]})
-    // } else if (this.state.searchMaxLength) {
-    //   this.setState({searchMaxLength: null})
-    // }
-    if (length > 10) {
-      this._fetchAutocompleteSuggestionsThrottledLonger(
-        event.target.value.trim()
-      )
-    } else if (length > 4) {
-      this._fetchAutocompleteSuggestionsThrottled(event.target.value.trim())
-    } else if (length) {
-      this._fetchAutocompleteSuggestions(event.target.value.trim())
-    } else {
-      this.setState({
-        autocompleteSuggestions: null,
-        autocompleteHighlight: -1,
-        showAutocompleteSuggestions: true
-      })
-    }
-  }
+    const q = event.target.value.trim();
+    this.setState({ q: event.target.value }, () => {
+      const length = q.length;
+      if (length > 5 || /\s/.test(q)) {
+        this.fetchAutocompleteSuggestionsThrottled(q, true);
+      } else if (length) {
+        this.fetchAutocompleteSuggestionsDebounced(q);
+      } else {
+        this.setState({
+          autocompleteSuggestions: null,
+          autocompleteSearchSuggestions: null,
+          autocompleteHighlight: -1,
+          showAutocompleteSuggestions: true,
+        });
+      }
+    });
+  };
 
-  _fetchAutocompleteSuggestions = q => {
-    let url = `https://songsear.ch/api/search/autocomplete?q=${q}`
-    const cached = this._fetchAutocompleteSuggestionsCache[q]
+  fetchAutocompleteSuggestions = (q, throttled = false) => {
+    let url = `${SERVER}/api/search/autocomplete?q=${q}`;
+    if (throttled) {
+      url += '&throttled';
+    }
+    const cached = this._fetchAutocompleteSuggestionsCache[q];
     if (cached) {
       return Promise.resolve(cached).then(results => {
         this.setState({
           autocompleteSuggestions: results.matches,
-          autocompleteHighlight: -1
-        })
-      })
+          autocompleteSearchSuggestions: results.search_suggestions,
+          autocompleteHighlight: -1,
+        });
+      });
     }
+    this.waitingFor = q;
     fetch(url).then(r => {
       if (r.status === 200) {
-        r.json().then(results => {
-          this._fetchAutocompleteSuggestionsCache[q] = results
-          this.setState({
-            autocompleteSuggestions: results.matches,
-            autocompleteHighlight: -1
-          })
-        })
+        if (q.startsWith(this.waitingFor)) {
+          r.json().then(results => {
+            this._fetchAutocompleteSuggestionsCache[q] = results;
+            this.setState({
+              autocompleteSuggestions: results.matches,
+              autocompleteSearchSuggestions: results.search_suggestions,
+              autocompleteHighlight: -1,
+            });
+          });
+        }
       }
-    })
-  }
-
-  _fetchAutocompleteSuggestionsThrottled = throttle(
-    500,
-    this._fetchAutocompleteSuggestions
-  )
-
-  _fetchAutocompleteSuggestionsThrottledLonger = throttle(
-    1500,
-    this._fetchAutocompleteSuggestions
-  )
+    });
+  };
 
   onKeyDownSearch = event => {
-    let suggestions = this.state.autocompleteSuggestions
+    let suggestions = this.state.autocompleteSuggestions;
     if (suggestions) {
-      let highlight = this.state.autocompleteHighlight
+      let highlight = this.state.autocompleteHighlight;
       if (event.key === 'Tab') {
-        event.preventDefault()
+        event.preventDefault();
         let suggestion =
-          highlight > -1 ? suggestions[highlight] : suggestions[0]
+          highlight > -1 ? suggestions[highlight] : suggestions[0];
         if (suggestion.append) {
           this.setState({
             q: appendSuggestion(this.state.q, suggestion.text) + ' ',
             autocompleteSuggestions: null,
-            autocompleteHighlight: -1
-          })
+            autocompleteHighlight: -1,
+          });
         } else {
           this.setState({
-            q: suggestion.text + ' '
-          })
-          this._fetchAutocompleteSuggestions(suggestion.text)
+            q: suggestion.text + ' ',
+          });
+          this.fetchAutocompleteSuggestions(suggestion.text);
         }
       } else if (event.key === 'ArrowDown' && highlight < suggestions.length) {
-        event.preventDefault()
-        this.setState({ autocompleteHighlight: highlight + 1 })
+        event.preventDefault();
+        this.setState({ autocompleteHighlight: highlight + 1 });
       } else if (event.key === 'ArrowUp' && highlight > -1) {
-        this.setState({ autocompleteHighlight: highlight - 1 })
+        this.setState({ autocompleteHighlight: highlight - 1 });
       } else if (event.key === 'Enter') {
         if (highlight > -1) {
-          event.preventDefault()
-          let suggestion =
-            highlight > -1 ? suggestions[highlight] : suggestions[0]
-          if (suggestion.append) {
+          event.preventDefault();
+          const searchSuggestions = this.state.autocompleteSearchSuggestions;
+          if (highlight === 0 && searchSuggestions && searchSuggestions.total) {
+            this._submit(this.state.q, 'search');
+            return;
+          }
+          highlight--;
+          if (suggestions[highlight]._url) {
             this.setState({
-              q: appendSuggestion(this.state.q, suggestion.text) + ' ',
+              // XXX perhaps there should be something that updates
+              // the state to say we're redirecting.
               autocompleteSuggestions: null,
-              autocompleteHighlight: -1
-            })
+              autocompleteHighlight: -1,
+            });
+            document.location.href = absolutifyUrl(suggestions[highlight]._url);
+            return;
           } else {
             this.setState(
               {
                 q: suggestions[highlight].text,
                 autocompleteSuggestions: null,
-                autocompleteHighlight: -1
+                autocompleteHighlight: -1,
               },
-              () => {
-                this._submit(suggestions[highlight].text, 'enterkey')
-              }
-            )
+              () => this._submit(this.state.q)
+            );
           }
+          // let suggestion =
+          //   highlight > -1 ? suggestions[highlight] : suggestions[0];
+          // if (suggestion.append) {
+          //   this.setState({
+          //     q: appendSuggestion(this.state.q, suggestion.text) + ' ',
+          //     autocompleteSuggestions: null,
+          //     autocompleteHighlight: -1,
+          //   });
+          // } else {
+          //   this.setState(
+          //     {
+          //       q: suggestions[highlight].text,
+          //       autocompleteSuggestions: null,
+          //       autocompleteHighlight: -1,
+          //     },
+          //     () => {
+          //       this._submit(suggestions[highlight].text, 'enterkey');
+          //     }
+          //   );
+          // }
         }
       }
     }
-  }
+  };
 
   onSelectSuggestion = (event, suggestion) => {
-    event.preventDefault()
-    this._submit(suggestion.text, 'clicked')
-  }
+    event.preventDefault();
+    if (suggestion._url) {
+      document.location.href = suggestion._url;
+      return;
+    }
+    let newText = suggestion.text;
+    if (suggestion.append) {
+      newText = appendSuggestion(this.state.q, newText);
+    }
+    this.setState(
+      {
+        q: newText,
+        autocompleteSuggestions: null,
+        autocompleteHighlight: -1,
+      },
+      () => {
+        this._submit(suggestion.text, 'clicked');
+      }
+    );
+  };
+
+  onSelectSuggestionAll = event => {
+    event.preventDefault();
+    this._submit(this.state.q, 'search');
+  };
 
   render() {
     return (
       <form
         style={{ margin: '50px 0' }}
-        action="https://songsear.ch/q/"
+        action={`${SERVER}/q/`}
         onSubmit={this.submitSearch}
       >
         <div
@@ -211,45 +293,148 @@ class App extends Component {
           />
           <i className="search icon" />
           {this.state.autocompleteSuggestions &&
-          this.state.showAutocompleteSuggestions
-            ? <ShowAutocompleteSuggestions
-                onSelectSuggestion={this.onSelectSuggestion}
-                highlight={this.state.autocompleteHighlight}
-                suggestions={this.state.autocompleteSuggestions}
-              />
-            : null}
+          this.state.showAutocompleteSuggestions ? (
+            <ShowAutocompleteSuggestions
+              q={this.state.q}
+              onSelectSuggestionAll={this.onSelectSuggestionAll}
+              onSelectSuggestion={this.onSelectSuggestion}
+              highlight={this.state.autocompleteHighlight}
+              suggestions={this.state.autocompleteSuggestions}
+              searchSuggestions={this.state.autocompleteSearchSuggestions}
+            />
+          ) : null}
         </div>
       </form>
-    )
+    );
   }
 }
 
-export default App
+export default App;
 
-const ShowAutocompleteSuggestions = ({
-  highlight,
-  suggestions,
-  onSelectSuggestion
-}) => {
-  if (!suggestions.length) {
-    return null
+class ShowAutocompleteSuggestions extends React.PureComponent {
+  componentDidMount() {
+    this._maybeConvertLazyLoadImages(this.props.suggestions);
   }
-  return (
-    <div className="autocomplete">
-      <ul>
-        {suggestions.map((s, index) => {
-          let className = index === highlight ? 'active' : ''
-          return (
+
+  componentWillReceiveProps(nextProps) {
+    this._maybeConvertLazyLoadImages(nextProps.suggestions);
+  }
+
+  _maybeConvertLazyLoadImages = suggestions => {
+    if (suggestions.some(suggestion => !!suggestion.id)) {
+      convertLazyLoadImages();
+    }
+  };
+
+  render() {
+    const {
+      q,
+      highlight,
+      suggestions,
+      searchSuggestions,
+      onSelectSuggestion,
+      onSelectSuggestionAll,
+    } = this.props;
+    if (!suggestions.length) {
+      return null;
+    }
+    return (
+      <div className="autocomplete">
+        <ul>
+          {searchSuggestions &&
+          searchSuggestions.total &&
+          searchSuggestions.capped ? (
             <li
-              key={index}
-              className={className}
-              onClick={e => onSelectSuggestion(e, s)}
+              onClick={onSelectSuggestionAll}
+              className={
+                highlight === 0
+                  ? 'active search-suggestion'
+                  : 'search-suggestion'
+              }
             >
-              <p dangerouslySetInnerHTML={{ __html: s.html }} />
+              {searchSuggestions.capped ? (
+                <a
+                  style={{ float: 'right' }}
+                  href={'/q/' + encodeURIComponent(searchSuggestions.term)}
+                >
+                  {numberWithCommas(searchSuggestions.total)}{' '}
+                  {searchSuggestions.desperate
+                    ? 'approximate matches'
+                    : 'good matches'}
+                </a>
+              ) : null}
+              <a href={'/q/' + encodeURIComponent(searchSuggestions.term)}>
+                Search for <i>{q}</i>
+              </a>
             </li>
-          )
-        })}
-      </ul>
-    </div>
-  )
+          ) : null}
+
+          {suggestions.map((s, index) => {
+            let className = index + 1 === highlight ? 'active' : '';
+            return (
+              <li
+                key={index}
+                className={className}
+                onClick={e => onSelectSuggestion(e, s)}
+              >
+                {s.id ? (
+                  <ShowAutocompleteSuggestionSong song={s} />
+                ) : (
+                  <p
+                    className="simple-autocomplete"
+                    dangerouslySetInnerHTML={{ __html: s.html }}
+                  />
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
+
+class ShowAutocompleteSuggestionSong extends React.PureComponent {
+  render() {
+    const { song } = this.props;
+    return (
+      <div className="media autocomplete-suggestion-song">
+        <div className="media-left">
+          <img
+            className={
+              song.image && song.image.preview
+                ? 'img-rounded lazyload preview'
+                : 'img-rounded lazyload'
+            }
+            src={
+              song.image && song.image.preview
+                ? song.image.preview
+                : placeholderImage
+            }
+            data-src={
+              song.image ? absolutifyUrl(song.image.url) : placeholderImage
+            }
+            alt={song.name}
+          />
+        </div>
+        <div className="media-body">
+          <h5 className="artist-name">
+            <b>{song.name}</b>
+            {' by '}
+            <span>{song.artist.name}</span>
+          </h5>
+          {song.fragments.map((fragment, i) => {
+            return <p key={i} dangerouslySetInnerHTML={{ __html: fragment }} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+/* http://stackoverflow.com/a/2901298 */
+const numberWithCommas = function(x) {
+  var parts = x.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
+};
