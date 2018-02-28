0675f6573f17c32595f34f7bd281878d2f29e1a1
Peter Bengtsson <mail@peterbe.com>
2018-02-28 11:06:42 -0500

    using Popen pipe instead

--- peterbecom/compressors.py
+++ peterbecom/compressors.py
@@ -1,8 +1,4 @@
-
-import os
-import tempfile
-
-import delegator
+import subprocess
 
 from pipeline.compressors import CompressorBase
 from django.conf import settings
@@ -11,27 +7,14 @@ from django.conf import settings
 class CSSOCompressor(CompressorBase):
 
     def compress_css(self, css):
-        # Feb 2018.
-        # The reason I chose to use a temporary directory and calling
-        # csso with a input file and an output file is because
-        # I was getting out-of-memory problems in docker.
-        with tempfile.TemporaryDirectory() as dir_:
-            input_fn = os.path.join(dir_, 'input.css')
-            with open(input_fn, 'w') as f:
-                f.write(css)
-            output_fn = os.path.join(dir_, 'output.css')
-            command = '{} {} {} --restructure-off'.format(
-                settings.PIPELINE['CSSO_BINARY'],
-                input_fn,
-                output_fn
-            )
-            r = delegator.run(command)
-            if r.return_code:
-                print(command)
-                raise Exception(r.return_code)
-            with open(output_fn) as f:
-                css_out = f.read()
-
+        proc = subprocess.Popen(
+            [settings.PIPELINE['CSSO_BINARY'], '--restructure-off'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        css_out = proc.communicate(
+            input=css.encode('utf-8')
+        )[0].decode('utf-8')
         # was_size = len(css)
         # new_size = len(css_out)
         # print('FROM {} to {} Saved {}  ({!r})'.format(
