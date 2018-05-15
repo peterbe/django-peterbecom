3e9d0af4316fcc06caa3e39896189cc150efffa7
Peter Bengtsson <mail@peterbe.com>
2018-05-15 11:39:07 -0400

    final fixes

--- reload_and_clear_cache.py
+++ reload_and_clear_cache.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import os
+import shutil
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peterbecom.settings")
 application = get_wsgi_application()
+
+
+cache_dir = 'peterbecom-static-content/_FSCACHE/plog'
+if os.path.isdir(cache_dir):
+    shutil.rmtree(cache_dir)
+    print('Deleted', cache_dir)
 
 from django.core.cache import cache  # noqa
 cache.clear()
