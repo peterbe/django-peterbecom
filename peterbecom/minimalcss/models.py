ef2e733b4e6182d86a830574caaea4728d13d9b2
Peter Bengtsson <mail@peterbe.com>
2018-05-23 16:26:32 -0400

    better error handling in minimization

--- peterbecom/minimalcss/models.py
+++ peterbecom/minimalcss/models.py
@@ -4,6 +4,7 @@ from django.contrib.postgres.fields import JSONField
 
 class Minimization(models.Model):
     url = models.URLField()
-    result = JSONField()
+    result = JSONField(null=True)
+    error = JSONField(null=True)
     time_took = models.FloatField()
     add_date = models.DateTimeField(auto_now_add=True)
