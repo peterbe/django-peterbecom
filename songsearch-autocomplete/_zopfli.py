f6598c2717d2a06a2ce254ee4662d316ac72651b
Peter Bengtsson <mail@peterbe.com>
2018-08-06 14:59:09 -0400

    songsearch-autocomplete with zopfli

---
+++ songsearch-autocomplete/_zopfli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.5
+
+import os
+import subprocess
+import time
+
+
+ZOPFLI_PATH = os.environ.get("ZOPFLI_PATH", "/usr/bin/zopfli")
+I = int(os.environ.get("ZOPFLI_ITERATIONS", 500))
+
+
+def run(file):
+    if not os.path.isfile(file + ".gz"):
+        cmd = [ZOPFLI_PATH, "--i{}".format(I), file]
+        t0 = time.time()
+        exit_code = subprocess.check_call(cmd, timeout=60)
+        t1 = time.time()
+        if exit_code:
+            print(" ".join(cmd))
+            print("FAILED TO GENERATE", file + ".gz")
+        else:
+            print("Created", os.path.basename(file + ".gz"))
+            print("Took", "{:.1f}s".format(t1 - t0))
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    for file in sys.argv[1:]:
+        run(file)
+    sys.exit(0)
--- songsearch-autocomplete/insert.sh
+++ songsearch-autocomplete/insert.sh
@@ -6,7 +6,13 @@ cd /home/django/django-peterbecom/songsearch-autocomplete
 rm -fr ../peterbecom-static-content/songsearch-autocomplete
 aunpack -X ../peterbecom-static-content/ songsearch-autocomplete.zip
 
+./_zopfli.py ../peterbecom-static-content/songsearch-autocomplete/css/*.css
+./_zopfli.py ../peterbecom-static-content/songsearch-autocomplete/js/*.js
+
 ./_insert.py
+
+./_zopfli.py ../peterbecom-static-content/_FSCACHE/plog/blogitem-040601-1/index.html
+
 echo "Finished at..."
 echo `date`
 echo ""
