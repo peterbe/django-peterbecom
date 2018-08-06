c66451a02629b8e7e70fd821a8a1df82065992be
Peter Bengtsson <mail@peterbe.com>
2018-08-06 15:15:45 -0400

    more tunings

--- songsearch-autocomplete/_zopfli.py
+++ songsearch-autocomplete/_zopfli.py
@@ -10,17 +10,19 @@ I = int(os.environ.get("ZOPFLI_ITERATIONS", 500))
 
 
 def run(file):
-    if not os.path.isfile(file + ".gz"):
-        cmd = [ZOPFLI_PATH, "--i{}".format(I), file]
-        t0 = time.time()
-        exit_code = subprocess.check_call(cmd, timeout=60)
-        t1 = time.time()
-        if exit_code:
-            print(" ".join(cmd))
-            print("FAILED TO GENERATE", file + ".gz")
-        else:
-            print("Created", os.path.basename(file + ".gz"))
-            print("Took", "{:.1f}s".format(t1 - t0))
+    if os.path.isfile(file + ".gz"):
+        print(file + ".gz already existed")
+        return 1
+    cmd = [ZOPFLI_PATH, "--i{}".format(I), file]
+    t0 = time.time()
+    exit_code = subprocess.check_call(cmd, timeout=60)
+    t1 = time.time()
+    if exit_code:
+        print(" ".join(cmd))
+        print("FAILED TO GENERATE", file + ".gz")
+    else:
+        print("Created", os.path.basename(file + ".gz"))
+        print("Took", "{:.1f}s".format(t1 - t0))
     return 0
 
 
--- songsearch-autocomplete/insert-local.sh
+++ songsearch-autocomplete/insert-local.sh
@@ -8,7 +8,6 @@ aunpack -X ../peterbecom-static-content/ songsearch-autocomplete.zip
 export ZOPFLI_PATH=/usr/local/bin/zopfli
 ./_zopfli.py ../peterbecom-static-content/songsearch-autocomplete/css/*.css
 ./_zopfli.py ../peterbecom-static-content/songsearch-autocomplete/js/*.js
-# rm -fr ../peterbecom-static-content/_FSCACHE/plog/blogitem-040601-1/index.html.gz
 
 ./_insert.py && \
   ./_zopfli.py ../peterbecom-static-content/_FSCACHE/plog/blogitem-040601-1/index.html
