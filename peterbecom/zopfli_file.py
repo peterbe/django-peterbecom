a5b8e3ed45abb8894495105b85ebd8f265bd6a61
Peter Bengtsson <mail@peterbe.com>
2018-08-08 13:47:53 -0400

    longer timeout

--- peterbecom/zopfli_file.py
+++ peterbecom/zopfli_file.py
@@ -7,7 +7,7 @@ class ZopfliException(Exception):
     """Happens when the zopfli command fails"""
 
 
-def zopfli_file(filepath, i=500, timeout=30):
+def zopfli_file(filepath, i=500, timeout=60):
     destination = filepath + ".gz"
     cmd = [settings.ZOPFLI_PATH, "--i{}".format(i), filepath]
     exit_code = subprocess.check_call(cmd, timeout=timeout)
