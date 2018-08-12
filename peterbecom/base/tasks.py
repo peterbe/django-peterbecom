5a0a73f50150c44ede7ac30e179e9ed2e133e305
Peter Bengtsson <mail@peterbe.com>
2018-08-11 23:29:36 -0400

    safer insert-songsearch-autocomplete

--- peterbecom/base/tasks.py
+++ peterbecom/base/tasks.py
@@ -52,16 +52,9 @@ def post_process_cached_html(filepath, url):
         print("mincss optimized {}".format(filepath))
         break
 
-    # Minification
     if not url.endswith("/plog/blogitem-040601-1"):
         minified_html = _minify_html(filepath, url)
-    else:
-        minified_html = None
 
-    # Zopfli
-    # At the moment, making an exception for the lyrics page because of the
-    # various problems had with the songsearch-autocomplete insertion.
-    if not url.endswith("/plog/blogitem-040601-1"):
         _zopfli_html(minified_html and minified_html or optimized_html, filepath, url)
 
 
