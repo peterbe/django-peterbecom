e1b3c81ba618b32683699e22c5a161dff2ae6f81
Peter Bengtsson <mail@peterbe.com>
2018-11-05 22:49:01 -0500

    plogadmin2

--- peterbecom/api/middleware.py
+++ peterbecom/api/middleware.py
@@ -1,4 +1,15 @@
-# from django.core.exceptions import PermissionDenied
+import backoff
+import requests
+
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+
+
+class OIDCEndpointRequestError(Exception):
+    """Happens when the server-to-server communication with the OIDC
+    endpoint succeeds but the OIDC endpoints responds with a status code
+    less than 500 and not equal to 200 or 401."""
 
 
 class AuthenticationMiddleware:
@@ -6,11 +17,42 @@ class AuthenticationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request.path)
-        request.csrf_processing_done = True
-        response = self.get_response(request)
+        if request.path.startswith("/api/"):
+            if request.method != "GET":
+                header_value = request.META.get("HTTP_AUTHORIZATION")
+                if not header_value:
+                    raise PermissionDenied("No HTTP_AUTHORIZATION")
+                print("HEADER_VALUE", repr(header_value))
+                access_token = header_value.split("Bearer")[1].strip()
+                user_info = self.fetch_oidc_user_profile(access_token)
+                if user_info:
+                    request.user = get_user_model().objects.get(
+                        email=user_info["email"]
+                    )
 
-        # Code to be executed for each request/response after
-        # the view is called.
+            request.csrf_processing_done = True
 
-        return response
+        return self.get_response(request)
+
+    @backoff.on_exception(
+        backoff.constant, requests.exceptions.RequestException, max_tries=5
+    )
+    def fetch_oidc_user_profile(self, access_token):
+        url = settings.OIDC_USER_ENDPOINT
+        response = requests.get(
+            url, headers={"Authorization": "Bearer {}".format(access_token)}
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            # The OIDC provider did not like the access token.
+            raise PermissionDenied("Unauthorized access token")
+        elif response.status_code >= 500:
+            raise requests.exceptions.RequestException(
+                "{} on {}".format(response.status_code, url)
+            )
+
+        # This could happen if, for some reason, we're not configured to be
+        # allowed to talk to the OIDC endpoint.
+        raise OIDCEndpointRequestError(response.status_code)
