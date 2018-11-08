107f158540ac243809425b952bbf80b7d4fd339a
Peter Bengtsson <mail@peterbe.com>
2018-11-07 21:18:09 -0500

    brotli the songsearch-autocomplete assets

--- songsearch-autocomplete/make_build.sh
+++ songsearch-autocomplete/make_build.sh
@@ -6,6 +6,8 @@ rm -fr build.zip
 pushd build
 time zopfli -i500 static/js/*.js
 time zopfli -i500 static/css/*.css
+time brotli static/js/*.js
+time brotli static/css/*.css
 mv static songsearch-autocomplete
 apack songsearch-autocomplete.zip songsearch-autocomplete
 mv songsearch-autocomplete.zip ..
