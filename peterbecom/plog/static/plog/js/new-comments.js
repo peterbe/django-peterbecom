4ad6f32440e5e0d400d9fa37f9f6bbcd6fb43421
Peter Bengtsson <mail@peterbe.com>
2018-11-12 21:07:24 -0500

    more patience with new-comments

--- peterbecom/plog/static/plog/js/new-comments.js
+++ peterbecom/plog/static/plog/js/new-comments.js
@@ -1,90 +1,94 @@
-$(function() {
-  $('.comments').on('click', '.delete-button, .approve-button', function(
-    event
-  ) {
-    event.preventDefault();
+(function() {
+  window.setTimeout(function() {
+    $('.comments').on('click', '.delete-button, .approve-button', function(
+      event
+    ) {
+      event.preventDefault();
 
-    $(this).addClass('loading');
-    $(this).addClass('disabled');
-    var button = $(this);
-    var url = button.data('url');
+      $(this).addClass('loading');
+      $(this).addClass('disabled');
+      var button = $(this);
+      var url = button.data('url');
 
-    $.post(url, {
-      csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-    })
-      .done(function() {
-        button
-          .parent('.approval')
-          .text(url.indexOf('/delete/') === -1 ? 'Approved' : 'Deleted');
+      $.post(url, {
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
       })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        throw new Error('Status ' + textStatus + ' errorThrown ' + errorThrown);
-      });
-  });
-
-  $('.comments').on('click', 'input.all-unchecked', function(event) {
-    var checkBoxes = $('.comment input[type="checkbox"]');
-    checkBoxes.prop('checked', !checkBoxes.prop('checked'));
-  });
-
-  var approveAllUrl = $('.comments').data('approve-all-url');
-  var deleteAllUrl = $('.comments').data('delete-all-url');
-  $('.comments').on('click', '.approve-all-button', function(event) {
-    event.preventDefault();
-    _batch(this, 'approve');
-  });
-
-  $('.comments').on('click', '.delete-all-button', function(event) {
-    event.preventDefault();
-    _batch(this, 'delete');
-  });
-
-  function _batch(button, action) {
-    var ids = [];
-    var formData = new FormData();
-    formData.append(
-      'csrfmiddlewaretoken',
-      $('input[name="csrfmiddlewaretoken"]').val()
-    );
-    $('input[name="ids"]:checked').each(function() {
-      var id = $(this).val();
-      $('button', $(this).parent())
-        .addClass('loading')
-        .addClass('disabled');
-      ids.push(id);
-    });
-    formData.append('ids', ids);
-    console.log('csrf', $('input[name="csrfmiddlewaretoken"]').val());
-    var url = action === 'approve' ? approveAllUrl : deleteAllUrl;
-    if (ids.length) {
-      fetch(url, {
-        method: 'POST',
-        body: formData,
-        credentials: 'same-origin'
-      })
-        .then(function(r) {
-          if (r.status !== 200) {
-            alert(r.status + ' - ' + r.statusText);
-            return;
-          }
-          return r
-            .json()
-            .then(function(result) {
-              $('input[name="ids"]:checked').each(function() {
-                $(this)
-                  .parent('.approval')
-                  .text(action === 'approve' ? 'Approved' : 'Deleted');
-              });
-            })
-            .catch(function(error) {
-              console.warn('ERROR 2');
-              console.error(error);
-            });
+        .done(function() {
+          button
+            .parent('.approval')
+            .text(url.indexOf('/delete/') === -1 ? 'Approved' : 'Deleted');
         })
-        .catch(function(error) {
-          console.warn('ERROR 1');
-          console.error(error);
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          throw new Error(
+            'Status ' + textStatus + ' errorThrown ' + errorThrown
+          );
         });
+    });
+
+    $('.comments').on('click', 'input.all-unchecked', function(event) {
+      var checkBoxes = $('.comment input[type="checkbox"]');
+      checkBoxes.prop('checked', !checkBoxes.prop('checked'));
+    });
+
+    var approveAllUrl = $('.comments').data('approve-all-url');
+    var deleteAllUrl = $('.comments').data('delete-all-url');
+    $('.comments').on('click', '.approve-all-button', function(event) {
+      event.preventDefault();
+      _batch(this, 'approve');
+    });
+
+    $('.comments').on('click', '.delete-all-button', function(event) {
+      event.preventDefault();
+      _batch(this, 'delete');
+    });
+
+    function _batch(button, action) {
+      var ids = [];
+      var formData = new FormData();
+      formData.append(
+        'csrfmiddlewaretoken',
+        $('input[name="csrfmiddlewaretoken"]').val()
+      );
+      $('input[name="ids"]:checked').each(function() {
+        var id = $(this).val();
+        $('button', $(this).parent())
+          .addClass('loading')
+          .addClass('disabled');
+        ids.push(id);
+      });
+      formData.append('ids', ids);
+      console.log('csrf', $('input[name="csrfmiddlewaretoken"]').val());
+      var url = action === 'approve' ? approveAllUrl : deleteAllUrl;
+      if (ids.length) {
+        fetch(url, {
+          method: 'POST',
+          body: formData,
+          credentials: 'same-origin'
+        })
+          .then(function(r) {
+            if (r.status !== 200) {
+              alert(r.status + ' - ' + r.statusText);
+              return;
+            }
+            return r
+              .json()
+              .then(function(result) {
+                $('input[name="ids"]:checked').each(function() {
+                  $(this)
+                    .parent('.approval')
+                    .text(action === 'approve' ? 'Approved' : 'Deleted');
+                });
+              })
+              .catch(function(error) {
+                console.warn('ERROR 2');
+                console.error(error);
+              });
+          })
+          .catch(function(error) {
+            console.warn('ERROR 1');
+            console.error(error);
+          });
+      }
     }
-  }
-});
+  }, 1000);
+})();
