f4974fe80ca7febcba80ca5211130904ae882120
Peter Bengtsson <mail@peterbe.com>
2018-11-27 20:42:39 -0500

    build scripts

---
+++ adminui/make_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INLINE_RUNTIME_CHUNK=false yarn run build
+
+rm -fr build.zip
+pushd build
+time zopfli -i500 static/js/*.js
+time zopfli -i500 static/css/*.css
+time brotli static/js/*.js
+time brotli static/css/*.css
+# mv static songsearch-autocomplete
+# apack songsearch-autocomplete.zip songsearch-autocomplete
+# mv songsearch-autocomplete.zip ..
+# mv songsearch-autocomplete static
+popd
+apack build.zip build
+# aunpack -l songsearch-autocomplete.zip
