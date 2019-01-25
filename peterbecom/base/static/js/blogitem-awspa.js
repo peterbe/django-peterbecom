97c356128d30769c0afb09f98468dfe323e2159f
Peter Bengtsson <mail@peterbe.com>
2019-01-25 12:28:18 -0500

    only display the awspa after images have been loaded

--- peterbecom/base/static/js/blogitem-awspa.js
+++ peterbecom/base/static/js/blogitem-awspa.js
@@ -1,49 +1,58 @@
 (function() {
   var container = $('#awspa');
 
-  function loadAwspa(url, hidefirst) {
-    if (hidefirst) {
-      container.hide();
-    }
+  function loadAwspa(url) {
     container.load(url, function() {
-      if (hidefirst) {
-        container.fadeIn(400);
-      }
-      var oid = document.location.pathname.split('/')[2];
-      if (window.sessionStorage) {
-        var loaded = JSON.parse(
-          window.sessionStorage.getItem('loadedawspa') || '{}'
-        );
-        var asins = [];
-        $('.item', container).each(function(i, item) {
-          asins.push('' + $(item).data('asin'));
-        });
-        loaded[oid] = asins;
-        window.sessionStorage.setItem('loadedawspa', JSON.stringify(loaded));
+      var imagesToLoad = 0;
 
-        if (asins.length) {
-          url = url.split('?')[0];
-          var newURL = url + '?' + $.param({ seen: asins }, true);
-          $('<button class="mini ui button">')
-            .addClass('refresh')
-            .data('prefetcher', 'no')
-            .text('Refresh products')
-            .on('click', function(event) {
-              event.preventDefault();
-              $(this)
-                .text('Refreshing...')
-                .addClass('disabled');
-              loadAwspa(newURL);
-            })
-            .appendTo(container);
-        }
+      $('img', container).each(function() {
+        imagesToLoad++;
+        var i = new Image();
+        i.onload = function() {
+          imagesToLoad--;
+          if (!imagesToLoad) {
+            container.hide().fadeIn(300);
+          }
+        };
+        i.src = this.src;
+      });
+
+      var asins = [];
+      $('.item', container).each(function(i, item) {
+        asins.push('' + $(item).data('asin'));
+      });
+      if (asins.length && window.sessionStorage) {
+        $('<button class="mini ui button">')
+          .addClass('refresh')
+          .data('prefetcher', 'no')
+          .text('Refresh products')
+          .on('click', function(event) {
+            event.preventDefault();
+            $(this)
+              .text('Refreshing...')
+              .addClass('disabled');
+
+            var loaded = JSON.parse(
+              window.sessionStorage.getItem('loadedawspa') || '{}'
+            );
+            var oid = document.location.pathname.split('/')[2];
+            loaded[oid] = asins;
+            window.sessionStorage.setItem(
+              'loadedawspa',
+              JSON.stringify(loaded)
+            );
+
+            var newURL =
+              url.split('?')[0] + '?' + $.param({ seen: asins }, true);
+            container.hide();
+            loadAwspa(newURL);
+          })
+          .appendTo(container);
       }
     });
   }
-  window.setTimeout(function() {
-    if ($) {
-      // only if jQuery has loaded
-      loadAwspa(document.location.pathname + '/awspa', true);
-    }
-  }, 200);
+  if ($) {
+    // only if jQuery has loaded
+    loadAwspa(document.location.pathname + '/awspa', true);
+  }
 })();
