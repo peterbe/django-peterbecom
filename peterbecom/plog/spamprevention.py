7b36876db4fb154f2c8575350a41df85044511fd
Peter Bengtsson <mail@peterbe.com>
2019-02-11 10:12:06 -0500

    spam prevention

---
+++ peterbecom/plog/spamprevention.py
@@ -0,0 +1,31 @@
+import re
+
+import bleach
+from django.conf import settings
+
+
+def contains_spam_url_patterns(text):
+    assert settings.SPAM_URL_PATTERNS, "Don't use it without some patterns"
+
+    html = bleach.clean(text)
+
+    problems = []
+
+    regex = re.compile(r"|".join(settings.SPAM_URL_PATTERNS))
+
+    def scrutinize_link(attrs, new, **kwargs):
+        href_key = (None, "href")
+        href = attrs[href_key]
+        if href.startswith("mailto:") or href.startswith("tel:"):
+            # Leave untouched
+            return
+        if not (href.startswith("http:") or href.startswith("https:")):
+            # Bail if it's not a HTTP URL, such as ssh:// or ftp://
+            return
+
+        found = regex.findall(href)
+        if found:
+            problems.append(found)
+
+    bleach.linkify(html, callbacks=[scrutinize_link])
+    return bool(problems)
