654f2170e2b8513beec3b3ba458a6ff7e29e516c
Peter Bengtsson <mail@peterbe.com>
2019-04-21 13:58:25 -0400

    tests for api tasks

--- peterbecom/api/tasks.py
+++ peterbecom/api/tasks.py
@@ -1,6 +1,8 @@
 from django.template import loader
 from django.core.mail import send_mail
 from django.contrib.sites.models import Site
+from django.urls import reverse
+from django.conf import settings
 from huey.contrib.djhuey import task
 
 from peterbecom.plog.models import BlogComment
@@ -15,19 +17,51 @@ def send_comment_reply_email(blogcomment_id):
 
     parent = blogcomment.parent
     tos = [parent.email]
-    from_ = "Peterbe.com <mail@peterbe.com>"
-    body = _get_comment_reply_body(blogcomment.blogitem, blogcomment, parent)
-    subject = "Peterbe.com: Reply to your comment"
+    blogitem = blogcomment.blogitem
+    if blogitem.oid == "blogitem-040601-1":
+        from_ = "Find song by lyrics <mail@peterbe.com>"
+        subject = "Find song by lyrics: Reply to your comment"
+    else:
+        from_ = "Peterbe.com <mail@peterbe.com>"
+        subject = "Peterbe.com: Reply to your comment"
+    body = _get_comment_reply_body(blogitem, blogcomment, parent)
     send_mail(subject, body, from_, tos)
 
 
 def _get_comment_reply_body(blogitem, blogcomment, parent):
-    base_url = "https://%s" % Site.objects.get_current().domain
+    page = get_comment_page(blogcomment)
+    if page > 1:
+        comment_url = reverse("blog_post", args=[blogitem.oid, page])
+    else:
+        comment_url = reverse("blog_post", args=[blogitem.oid])
+
+    domain = Site.objects.get_current().domain
+    if domain == "peterbecom.local":
+        # Useful for local dev
+        base_url = "http://" + domain
+    else:
+        base_url = "https://" + domain
     template = loader.get_template("plog/comment_reply_body.txt")
     context = {
         "post": blogitem,
         "comment": blogcomment,
         "parent": parent,
         "base_url": base_url,
+        "comment_url": comment_url,
     }
     return template.render(context).strip()
+
+
+def get_comment_page(blogcomment):
+    root_comment = blogcomment
+    while root_comment.parent_id:
+        root_comment = root_comment.parent
+
+    qs = BlogComment.objects.filter(blogitem=blogcomment.blogitem, parent__isnull=True)
+    ids = list(qs.order_by("-add_date").values_list("id", flat=True))
+    per_page = settings.MAX_RECENT_COMMENTS
+    for i in range(settings.MAX_BLOGCOMMENT_PAGES):
+        sub_list = ids[i * per_page : (i + 1) * per_page]
+        if root_comment.id in sub_list:
+            return i + 1
+    return 1
---
+++ peterbecom/api/test_tasks.py
@@ -0,0 +1,112 @@
+import datetime
+
+import pytest
+from django.core import mail
+from django.urls import reverse
+from django.contrib.sites.models import Site
+from django.utils import timezone
+
+from peterbecom.plog.models import BlogItem, BlogComment
+from peterbecom.api import tasks
+
+
+@pytest.mark.django_db
+def test_send_comment_reply_email_page_1():
+
+    blogitem = BlogItem.objects.create(
+        oid="myoid",
+        title="TITLEX",
+        text="Test test",
+        display_format="markdown",
+        pub_date=timezone.now() - datetime.timedelta(seconds=10),
+    )
+    parent_comment = BlogComment.objects.create(
+        oid=BlogComment.next_oid(),
+        blogitem=blogitem,
+        name="Rooter",
+        email="rooter@example.com",
+        comment="This is the root comment",
+        approved=True,
+    )
+    blogcomment = BlogComment.objects.create(
+        oid=BlogComment.next_oid(),
+        blogitem=blogitem,
+        comment="Ma reply!",
+        approved=True,
+        parent=parent_comment,
+    )
+    tasks.send_comment_reply_email(blogcomment.id)
+
+    sent = mail.outbox[-1]
+    assert "Reply to your comment" in sent.subject
+    assert sent.to == ["rooter@example.com"]
+
+    comment_absolute_url = "https://" + Site.objects.get_current().domain
+    comment_absolute_url += reverse("blog_post", args=[blogitem.oid])
+    comment_absolute_url += "#" + parent_comment.oid
+    assert comment_absolute_url in sent.body
+
+
+@pytest.mark.django_db
+def test_send_comment_reply_email_page_2(settings):
+    settings.MAX_RECENT_COMMENTS = 10
+    blogitem = BlogItem.objects.create(
+        oid="myoid",
+        title="TITLEX",
+        text="Test test",
+        display_format="markdown",
+        pub_date=timezone.now() - datetime.timedelta(seconds=10),
+    )
+    bulk = []
+    max_ = settings.MAX_RECENT_COMMENTS
+    for i in range(max_):
+        comment = "Comment number {}".format(i + 1)
+        bulk.append(
+            BlogComment(
+                oid=BlogComment.next_oid(),
+                blogitem=blogitem,
+                name="Rooter",
+                email="rooter@example.com",
+                comment=comment,
+                comment_rendered=comment,
+                approved=True,
+                add_date=timezone.now() - datetime.timedelta(seconds=1, hours=max_ - i),
+            )
+        )
+    BlogComment.objects.bulk_create(bulk)
+
+    parent_comment = BlogComment.objects.create(
+        oid=BlogComment.next_oid(),
+        blogitem=blogitem,
+        name="Rooter",
+        email="rooter@example.com",
+        comment="This is the root comment",
+        approved=True,
+        # Make it so old that it goes into page 2
+        add_date=timezone.now() - datetime.timedelta(seconds=1, hours=max_ + 1),
+    )
+    middle_blogcomment = BlogComment.objects.create(
+        oid=BlogComment.next_oid(),
+        blogitem=blogitem,
+        comment="First reply!",
+        email="middle@example.com",
+        approved=True,
+        parent=parent_comment,
+    )
+    blogcomment = BlogComment.objects.create(
+        oid=BlogComment.next_oid(),
+        blogitem=blogitem,
+        comment="Deeper reply",
+        approved=True,
+        parent=middle_blogcomment,
+    )
+    tasks.send_comment_reply_email(blogcomment.id)
+
+    sent = mail.outbox[-1]
+    assert "Reply to your comment" in sent.subject
+    assert sent.to == ["middle@example.com"]
+
+    comment_absolute_url = "https://" + Site.objects.get_current().domain
+    comment_absolute_url += reverse("blog_post", args=[blogitem.oid, 2])
+    comment_absolute_url += "#" + middle_blogcomment.oid
+    assert comment_absolute_url in sent.body
