bb1c69fc27b7520709d4b3a792df9e014f64962d
Peter Bengtsson <mail@peterbe.com>
2019-06-01 22:51:38 -0400

    late

--- peterbecom/base/geo.py
+++ peterbecom/base/geo.py
@@ -9,15 +9,23 @@ import os
 
 assert settings.GEOIP_PATH
 print(os.listdir(os.path.dirname(settings.GEOIP_PATH)))
+print(
+    os.path.basename(settings.GEOIP_PATH)
+    in os.listdir(os.path.dirname(settings.GEOIP_PATH))
+)
 assert os.path.isfile(settings.GEOIP_PATH), settings.GEOIP_PATH
 
-geoip_looker_upper = GeoIP2()
+geoip_looker_upper = None
 
 
 @lru_cache()
 def ip_to_city(ip_address):
     if ip_address == "127.0.0.1":
         return
+    # Initialize late
+    global geoip_looker_upper
+    if geoip_looker_upper is None:
+        geoip_looker_upper = GeoIP2()
     try:
         return geoip_looker_upper.city(ip_address)
     except AddressNotFoundError:
