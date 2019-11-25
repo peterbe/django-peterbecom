aad09109943861553d28c71884b5391f4449766e
Peter Bengtsson <mail@peterbe.com>
2019-11-25 14:54:11 -0500

    CDN_MAX_PURGE_URLS

--- peterbecom/base/models.py
+++ peterbecom/base/models.py
@@ -128,7 +128,7 @@ class CDNPurgeURL(models.Model):
     @classmethod
     def get(cls, max_urls=None):
         if not max_urls:
-            max_urls = getattr(settings, "CDN_MAX_PURGE_URLS", 10)
+            max_urls = settings.CDN_MAX_PURGE_URLS
         qs = cls.objects.filter(cancelled__isnull=True, processed__isnull=True)
         return list(qs.order_by("created")[:max_urls].values_list("url", flat=True))
 
