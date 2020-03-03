09d03337ab0e37bc1b343355969d284030119a37
Peter Bengtsson <mail@peterbe.com>
2020-03-02 21:36:34 -0500

    fixing things maybe

--- peterbecom/cache_backends.py
+++ peterbecom/cache_backends.py
@@ -3,6 +3,9 @@ from contextlib import contextmanager
 from django.core.cache.backends.locmem import LocMemCache
 
 
+# IS THIS EVEN USED ANYWHERE??
+
+
 class LockMemCache(LocMemCache):
     """Really dumb extension of LocMemCache that has a `lock` context manager."""
 
