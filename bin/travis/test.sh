7e2f0e823e0289db40e4131a25451d9021633cea
Peter Bengtsson <mail@peterbe.com>
2020-03-19 08:59:08 -0400

    tidying up

--- bin/travis/test.sh
+++ bin/travis/test.sh
@@ -2,14 +2,25 @@
 # pwd is the git repo.
 set -e
 
-# Make sure we're running ES 5
+# Make sure we're running Elasticsearch
 curl -v http://localhost:9200/
 
 pytest peterbecom
 
 
-# See comment in install.sh for why this commented out.
-# # Go into the adminui directory and expect to be able to run yarn run build
+# Go into the adminui directory and expect to be able to run yarn run build
 pushd adminui
 yarn run build
+
+# If the webpack stuff didn't work, it would only have created...
+#
+#   - 2.fdb5cd4e.chunk.js(.map)
+#   - main.d8a9c6c6.chunk.js(.map)
+#   - runtime-main.62a7d8f7.js(.map)
+#
+# If the webpack stuff did work there'll be lots of other files with higher
+# numbers beyond just 2.
+ls -l build/static/js/3.*
+
+
 popd
