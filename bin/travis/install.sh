0ab4e95e725df5341cfca5d247816c107d952145
Peter Bengtsson <mail@peterbe.com>
2020-03-19 09:22:15 -0400

    travis test adminui build (#988)
    * travis test adminui build
    
    * debug versions
    
    * whatever is the latest dist
    
    * call it 'redis'
    
    * not specifying jre
    
    * xenial
    
    * tidying up

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -12,12 +12,21 @@ echo "Install latest flake8"
 # Perhaps this should be in some dev-requirements.txt but YOLO
 pip install flake8
 
+echo "Version of node"
+node --version
+
+echo "Version of yarn"
+yarn --version
+
 echo "Installing the node packages"
 yarn
 
-# Commented out as of Dec 2018 because the version of nodejs I get is too old.
-# Switch to Xenial opened up other errors. Too lazy to fix this now.
-# echo "Install packages for adminui"
-# pushd adminui
-# yarn
-# popd
+echo "Install packages for adminui"
+pushd adminui
+yarn
+popd
+
+echo "Install packages for pulse"
+pushd pulse
+yarn
+popd
