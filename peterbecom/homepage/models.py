5004d3cda63b4c8a177c26c583bdc758b82f6908
Peter Bengtsson <mail@peterbe.com>
2021-02-03 09:04:43 -0500

    llast_referer (#1366)

--- peterbecom/homepage/models.py
+++ peterbecom/homepage/models.py
@@ -6,12 +6,14 @@ from django.db.models import F
 class CatchallURL(models.Model):
     path = models.CharField(max_length=400, unique=True)
     count = models.PositiveIntegerField(default=1)
+    last_referer = models.URLField(max_length=400, null=True)
     created = models.DateTimeField(auto_now_add=True)
     last_seen = models.DateTimeField(auto_now=True)
 
     @classmethod
-    def upsert(cls, path):
-        if not cls.objects.filter(path=path).update(
-            count=F("count") + 1, last_seen=timezone.now()
-        ):
+    def upsert(cls, path, last_referer=None):
+        update = {"count": F("count") + 1, "last_seen": timezone.now()}
+        if last_referer:
+            update["last_referer"] = last_referer[:400]
+        if not cls.objects.filter(path=path).update(**update):
             cls.objects.create(path=path)
