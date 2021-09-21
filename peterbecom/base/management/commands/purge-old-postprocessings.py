083b79e78f473fad2d6d2cb104cf5e113cb8b871
Peter Bengtsson <mail@peterbe.com>
2021-09-21 09:12:40 -0400

    delete unused management commands

--- peterbecom/base/management/commands/purge-old-postprocessings.py
+++
@@ -1,9 +0,0 @@
-from peterbecom.base.basecommand import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, **options):
-        raise NotImplementedError(
-            "Use the periodic task in "
-            "peterbecom.base.tasks.purge_old_postprocessings instead!!!"
-        )
