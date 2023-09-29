9acf91d6bdb3bfffcbff17a4c84f5aa6f1b4db94
Peter Bengtsson <mail@peterbe.com>
2023-09-29 11:22:06 -0400

    No need for directory if bunx works (#2268)

--- peterbecom/settings/base.py
+++ peterbecom/settings/base.py
@@ -489,5 +489,4 @@ USE_ES_SYNONYM_FILE_NAME = config("USE_ES_SYNONYM_FILE_NAME", default=True, cast
 # too old and can be deleted.
 MIN_RARELY_SEEN_CATCHALL_DAYS = 30
 
-HYLITE_DIRECTORY = None
-HYLITE_COMMAND = "bun run dist/index.js"
+HYLITE_COMMAND = "bunx hylite"
