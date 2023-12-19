a903fc76c1c361f5c754a9c3eea0d6776fda0c55
Peter Bengtsson <mail@peterbe.com>
2023-12-19 16:00:57 -0500

    Move thumbnail function (#2334)
    * move thumbnail function
    
    * remove also

--- peterbecom/base/templatetags/jinja_helpers.py
+++ peterbecom/base/templatetags/jinja_helpers.py
@@ -1,13 +1,10 @@
-import time
 import json
 
 import jinja2
 
-from django.db.utils import IntegrityError
 from django.urls import reverse
 
 from django_jinja import library
-from sorl.thumbnail import get_thumbnail
 
 
 @library.global_function
@@ -19,30 +16,6 @@ def url(viewname, *args, **kwargs):
 @library.global_function
 def thousands(n):
     return format(n, ",")
-
-
-@library.global_function
-def thumbnail(imagefile, geometry, **options):
-    if not options.get("format"):
-        # then let's try to do it by the file name
-        filename = imagefile
-        if hasattr(imagefile, "name"):
-            # it's an ImageFile object
-            filename = imagefile.name
-        if filename.lower().endswith(".png"):
-            options["format"] = "PNG"
-        elif filename.lower().endswith(".gif"):
-            pass
-        else:
-            options["format"] = "JPEG"
-    try:
-        return get_thumbnail(imagefile, geometry, **options)
-    except IntegrityError:
-        # The write is not transactional, and since this is most likely
-        # used in a write-view, we might get conflicts trying to write and a
-        # remember. Just try again a little bit later.
-        time.sleep(1)
-        return thumbnail(imagefile, geometry, **options)
 
 
 @library.global_function
