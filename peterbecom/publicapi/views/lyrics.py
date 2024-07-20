1b5f4389486c5b87741221042d4e82e719d25d4d
Peter Bengtsson <mail@peterbe.com>
2024-07-20 16:50:06 -0400

    treat 400 error differently (#2470)
    * treat 400 error differently
    
    * remove print

--- peterbecom/publicapi/views/lyrics.py
+++ peterbecom/publicapi/views/lyrics.py
@@ -30,6 +30,8 @@ def search(request):
     remote_url = f"{settings.LYRICS_REMOTE}/api/search?{urlencode(sp)}"
     response = requests_retry_session().get(remote_url)
     if response.status_code != 200:
+        if response.status_code == 400:
+            return http.JsonResponse(response.json(), status=response.status_code)
         return http.JsonResponse(
             {"error": "Unexpected proxy response code"}, status=response.status_code
         )
