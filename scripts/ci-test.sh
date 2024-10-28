949d3ee153c78536cc4424bcadc71032965a529a
Peter Bengtsson <mail@peterbe.com>
2024-10-28 11:53:58 -0400

    Test with coverage

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -40,4 +40,4 @@ SETTINGS
 # Make sure we're running Elasticsearch
 curl -v http://localhost:9200/
 
-uv run pytest peterbecom
+uv run pytest peterbecom --cov=peterbecom --cov-report xml:coverage.xml
