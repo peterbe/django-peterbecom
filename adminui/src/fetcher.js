86fe343df3254d730e4c33c8f18091b3159dadfb
Peter Bengtsson <peterbe@github.com>
2024-11-20 08:32:59 -0500

    remove adminui (#2627)
    * remove adminui
    
    * tidying

--- adminui/src/fetcher.js
+++
@@ -1,7 +0,0 @@
-export const fetcher = async (url) => {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`${response.status} on ${url}`);
-  }
-  return await response.json();
-};
