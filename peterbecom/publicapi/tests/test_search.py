20f6d726d14cbdce1e327b08be37fab1433ed0ce
Peter Bengtsson <mail@peterbe.com>
2024-12-10 15:02:59 -0500

    test search

--- peterbecom/publicapi/tests/test_search.py
+++ peterbecom/publicapi/tests/test_search.py
@@ -1,5 +1,10 @@
+import time
+
 import pytest
 from django.urls import reverse
+from django.utils import timezone
+
+from peterbecom.plog.models import BlogItem, Category
 
 
 @pytest.mark.django_db
@@ -31,6 +36,34 @@ def test_search_empty(client):
 
 
 @pytest.mark.django_db
+def test_search_happy_path(client):
+    blogitem = BlogItem.objects.create(
+        oid="hello-world",
+        title="Hello World",
+        pub_date=timezone.now(),
+        proper_keywords=["one", "two"],
+        display_format="markdown",
+        text="Hello *world*\n<!--split-->Second part",
+    )
+    category = Category.objects.create(name="Code")
+    blogitem.categories.add(category)
+    blogitem.save()
+    time.sleep(1)
+    url = reverse("publicapi:search")
+    response = client.get(url, {"q": "hello "})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["q"] == "hello"
+    assert data["results"]["count_documents"] == 1
+    documents = data["results"]["documents"]
+    assert documents[0]["oid"] == "hello-world"
+    assert documents[0]["categories"] == ["Code"]
+    assert not documents[0]["comment"]
+    assert documents[0]["date"]
+    assert documents[0]["title"] == "<mark>Hello</mark> World"
+
+
+@pytest.mark.django_db
 def test_autocomplete_empty(client):
     url = reverse("publicapi:autocomplete")
     response = client.get(url)
@@ -40,3 +73,44 @@ def test_autocomplete_empty(client):
     data = response.json()
     assert data["meta"]["found"] == 0
     assert data["results"] == []
+
+
+@pytest.mark.django_db
+def test_typeahead_happy_path(client):
+    url = reverse("publicapi:typeahead")
+    response = client.get(url, {"q": "p"})
+    assert response.status_code == 200
+    assert "public" in response.headers["cache-control"]
+    data = response.json()
+    assert data["meta"]["found"] > 0
+    assert data["meta"]["took"]
+    assert data["results"]
+    assert data["results"][0]["highlights"]
+    assert data["results"][0]["term"]
+
+
+@pytest.mark.django_db
+def test_typeahead_multiword(client):
+    url = reverse("publicapi:typeahead")
+    response = client.get(url, {"q": "peter s"})
+    assert response.status_code == 200
+    data = response.json()
+    assert "public" in response.headers["cache-control"]
+    assert data["meta"]["found"] > 0
+    assert data["meta"]["took"]
+    assert data["results"]
+    assert data["results"][0]["highlights"]
+    assert data["results"][0]["term"]
+
+
+@pytest.mark.django_db
+def test_typeahead_failing(client):
+    url = reverse("publicapi:typeahead")
+    response = client.get(url)
+    assert response.status_code == 400
+    response = client.get(url, {"q": "x" * 100})
+    assert response.status_code == 400
+    response = client.get(url, {"q": "x", "n": "100"})
+    assert response.status_code == 400
+    response = client.get(url, {"q": "x", "n": "xx"})
+    assert response.status_code == 400
