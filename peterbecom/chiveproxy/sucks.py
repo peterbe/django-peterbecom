03550392a3a637f78fc8f2d7b7e9a55256d6fbe5
Peter Bengtsson <mail@peterbe.com>
2025-05-18 17:49:07 -0400

    Go back to puppeteer

--- peterbecom/chiveproxy/sucks.py
+++ peterbecom/chiveproxy/sucks.py
@@ -1,32 +1,41 @@
 import hashlib
 import re
 
-from cachetools import TTLCache, cached
+import pyquery
 from django.conf import settings
-from scrapling.fetchers import Fetcher
+from django.core.cache import cache
 
-long_ttl_cache = TTLCache(maxsize=50, ttl=60 * 60)
-short_ttl_cache = TTLCache(maxsize=10, ttl=60)
+from . import puppeteer
 
 
 def make_it_more_iso(datestr):
     return re.sub(r"\b(\d)\b", r"0\1", datestr)
 
 
-@cached(cache=short_ttl_cache)
 def get_cards(limit=None, debug=False):
     base = "https://thechive.com/"
-    page = Fetcher.get(base, stealthy_headers=True)
-    assert page.status == 200, page.status
+    html = puppeteer.suck(base)
+    assert html, base
+    assert html.strip().endswith("</html>"), (base, html)
+    doc = pyquery.PyQuery(html)
+
+    if debug:
+        for title in doc("title").items():
+            print("TITLE", title.text())
+            break
+        else:
+            print("no title!")
+        # for element in doc("h1.card-title").items():
+        #     print("h1", element.text())
 
     count = 0
-    for slot in page.css("div.slot"):
-        for a in slot.css("a.card-img-link"):
-            href = a.attrib["href"]
+    for slot in doc("div.slot").items():
+        for a in slot("a.card-img-link").items():
+            href = a.attr("href")
             if not href.startswith(base):
                 continue
-            for img_element in a.css("img.card-thumb"):
-                img = img_element.attrib["src"]
+            for img in a("img.card-thumb").items():
+                img = img.attr("src")
                 break
             else:
                 continue
@@ -35,9 +44,8 @@ def get_cards(limit=None, debug=False):
             continue
 
         assert img.startswith("https"), img
-        for a in slot.css("h1.post-title a"):
-            text = a.text.replace("\xa0", " ").strip()
-
+        for a in slot("h1.post-title a").items():
+            text = a.text().replace("\xa0", " ").strip()
             if text.lower().endswith("(video)"):
                 continue
             if text.lower().endswith("(vote)"):
@@ -48,14 +56,13 @@ def get_cards(limit=None, debug=False):
 
         date = None
         human_time = None
-        for time_ in slot.css("time[datetime]"):
-            date = time_.attrib["datetime"]
+        for time_ in slot("time[datetime]").items():
+            date = time_.attr("datetime")
             date = make_it_more_iso(date)
-            human_time = time_.get_all_text().split("\n")[0].strip()
+            human_time = time_.text()  # Not needed?
             break
 
         uri = hashlib.md5(href.encode("utf-8")).hexdigest()[:8]
-        assert date
         yield {
             "url": href,
             "uri": uri,
@@ -70,15 +77,31 @@ def get_cards(limit=None, debug=False):
             break
 
 
-@cached(cache=long_ttl_cache)
 def get_card(url):
     assert url.startswith("https://thechive.com"), url
 
-    page = Fetcher.get(url, stealthy_headers=True)
-    assert page.status == 200, page.status
+    # The cache is really just to assure we don't run it more than once
+    # in a short timeframe. ...by some accident or local dev.
+    puppeteer_cache_key = "puppeteer_sucks:{}".format(
+        hashlib.md5(url.encode("utf-8")).hexdigest()
+    )
+    html = cache.get(puppeteer_cache_key)
+    if html is None:
+        print("Sucking", url)
+        html = puppeteer.suck(url)
+        assert html, url
+        assert html.strip().endswith("</html>"), (url, html)
+        print("Sucked", url)
+        if html:
+            cache.set(puppeteer_cache_key, html, 60)
+    else:
+        print("No need sucking", url, "(cached)")
 
-    for h1 in page.css("h1#post-title"):
-        text = h1.text.replace("\xa0", " ").strip()
+    doc = pyquery.PyQuery(html)
+
+    for h1 in doc("h1#post-title").items():
+        text = h1.text()
+        text = text.replace("\xa0", " ").strip()
         break
     else:
         print("NO 'h1#post-title'", url)
@@ -87,47 +110,51 @@ def get_card(url):
         return
 
     date = None
-    for time_ in page.css("header.article-header time[datetime]"):
-        date = time_.attrib["datetime"]
+    for time_ in doc("header.article-header time[datetime]").items():
+        date = time_.attr("datetime")
         break
 
     pictures = []
-    for figure in page.css("figure.gallery-item"):
+    # for figure in doc("div.gallery figure.gallery-item").items():
+    for figure in doc("figure.gallery-item").items():
         caption = []
         caption_html = []
         gifsrc = None
         mp4src = None
         src = None
-        for p in figure.css("figcaption.gallery-caption p"):
-            caption.append(p.text)
-            caption_html.append(p.get_all_text())
-        for img in figure.css("img.attachment-gallery-item-full"):
-            src = img.attrib["src"]
-            # print(dir(img))
-            # print([x for x in dir(img) if "attr" in x])
-            gifsrc = img.attrib.get("data-gifsrc")
+        for p in figure("figcaption.gallery-caption p").items():
+            caption.append(p.text())
+            caption_html.append(p.html())
+        for img in figure("img.attachment-gallery-item-full").items():
+            src = img.attr("src")
+            gifsrc = img.attr("data-gifsrc")
             break
         else:
-            for img in figure.css("img.attachment-gallery-item-hires"):
-                src = img.attrib["src"]
+            for img in figure("img.attachment-gallery-item-hires").items():
+                src = img.attr("src")
                 break
         if not src:
-            for video in figure.css("video.video-gif"):
-                for source in video.css("source"):
-                    if source.attrib["type"] == "video/mp4":
-                        mp4src = source.attrib["src"]
+            for video in figure("video.video-gif").items():
+                for source in video("source").items():
+                    if source.attr("type") == "video/mp4":
+                        mp4src = source.attr("src")
                     break
                 if mp4src:
-                    src = video.attrib["poster"]
+                    src = video.attr("poster")
                 break
+
         if not src:
             # Happens sometimes when it's just a bunch of Twitter quotes.
-            print(f"NO PICTURES (in {url}) figure", figure.html_string)
+            # if settings.DEBUG:
+            #     raise Exception("No src on {}".format(url))
+            print(f"NO PICTURES (in {url}) figuree", figure)
             continue
 
         src = src.replace("http://", "https://")
+        # assert src.startswith("https://"), src
         if gifsrc:
             gifsrc = gifsrc.replace("http://", "https://")
+            # assert gifsrc.startswith("https://")
 
         combined_caption = "\n".join(
             [x.strip() for x in caption if x.replace("\xa0", " ").strip()]
