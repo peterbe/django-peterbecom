a5b9d065ee269904d42dbb18cf09726fd1920914
Peter Bengtsson <mail@peterbe.com>
2025-07-16 23:19:32 -0400

    record publicapi-pageviews

--- peterbecom/base/middleware.py
+++ peterbecom/base/middleware.py
@@ -1,7 +1,13 @@
 import re
 import time
+import uuid
 
 from django import http
+from django.conf import settings
+
+from peterbecom.base.models import create_event
+from peterbecom.base.utils import fake_ip_address
+from peterbecom.publicapi.views.events import get_bot_analysis
 
 max_age_re = re.compile(r"max-age=(\d+)")
 
@@ -16,6 +22,65 @@ class StatsMiddleware:
         t1 = time.time()
         duration = t1 - t0
         response["X-Response-Time"] = int(duration * 1000)
+        return response
+
+
+class PublicAPIPageviewsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        t0 = time.time()
+        response = self.get_response(request)
+        t1 = time.time()
+        duration = t1 - t0
+        if request.path.startswith("/api/v1/"):
+            data = {
+                "duration": duration,
+                "method": request.method,
+            }
+            query_string = request.META.get("QUERY_STRING", "")
+            meta = {
+                "user_agent": request.META.get("HTTP_USER_AGENT"),
+                "path": request.path,
+                "query_string": query_string,
+                "request_url": request.build_absolute_uri(),
+            }
+            ua = meta.get("user_agent")
+            if ua:
+                is_bot, bot_agent = get_bot_analysis(ua)
+                data["bot_agent"] = bot_agent
+                data["is_bot"] = is_bot
+
+            ip_address = request.headers.get("x-forwarded-for") or request.META.get(
+                "REMOTE_ADDR"
+            )
+            if (
+                ip_address == "127.0.0.1"
+                and settings.DEBUG
+                and request.get_host().endswith("127.0.0.1:8000")
+            ):
+                ip_address = fake_ip_address(str(time.time()))
+            if ip_address:
+                meta["ip_address"] = ip_address
+
+            url = request.path
+            if query_string:
+                url += f"?{query_string}"
+
+            try:
+                create_event(
+                    type="publicapi-pageview",
+                    uuid=str(uuid.uuid4()),
+                    url=url,
+                    meta=meta,
+                    data=data,
+                )
+            except Exception as err:
+                if settings.DEBUG:
+                    raise err
+                print(f"WARNING! Unable to save 'publicapi-pageview': {err}")
+
         return response
 
 
