8b9f7755664c312d78031f1c6d052aff3706674c
Your Name <you@example.com>
2025-12-05 08:19:28 -0500

    django 6 and html emails on replies

--- peterbecom/api/tasks.py
+++ peterbecom/api/tasks.py
@@ -1,7 +1,9 @@
 import textwrap
 
+from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from huey.contrib.djhuey import task
 
 from peterbecom.plog.models import BlogComment
@@ -25,7 +27,15 @@ def send_comment_reply_email(blogcomment_id):
         from_ = "Peterbe.com <mail@peterbe.com>"
         subject = "Peterbe.com: Reply to your comment"
     body = _get_comment_reply_body(blogitem, blogcomment, parent)
-    send_mail(subject, body, from_, tos)
+    html_body = _get_html_comment_body(blogitem, blogcomment, parent)
+
+    if settings.DEBUG:
+        fname = f"/tmp/reply-comment-email.{blogcomment_id}.html"
+        with open(fname, "w") as f:
+            f.write(html_body)
+        print(f"Dumped HTML to {fname}")
+
+    send_mail(subject, body, from_, tos, html_message=html_body)
 
 
 def _get_comment_reply_body(blogitem, blogcomment, parent):
@@ -58,4 +68,31 @@ To visit the page again or to respond, go to:
 def line_indent(text, indent=" " * 4):
     return "\n".join(
         textwrap.wrap(text, initial_indent=indent, subsequent_indent=indent)
+    )
+
+
+def _get_html_comment_body(blogitem, blogcomment, parent):
+    base_url = f"https://{Site.objects.get_current().domain}"
+    if "peterbecom.local" in base_url:
+        base_url = "http://localhost:4000"
+
+    post_url = blog_post_url(blogitem.oid)
+    full_post_url = f"{base_url}{post_url}"
+
+    comment_url = f"{post_url}/comment/{parent.oid}"
+    full_comment_url = f"{base_url}{comment_url}"
+
+    parent_comment_date_human = parent.add_date.strftime("%b %d, %Y")
+
+    return render_to_string(
+        "emails/reply-comment-email.djt",
+        {
+            "blogitem": blogitem,
+            "is_lyrics_post": blogitem.oid == "blogitem-040601-1",
+            "comment_url": full_comment_url,
+            "post_url": full_post_url,
+            "blogcomment": blogcomment,
+            "parent": parent,
+            "parent_comment_date_human": parent_comment_date_human,
+        },
     )
