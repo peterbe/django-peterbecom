d34f0d121a4c36fda36934cf4b43fed11fbf5219
Your Name <you@example.com>
2026-07-24 09:01:28 -0400

    improve download error handling

--- scripts/nltk-downloads.py
+++ scripts/nltk-downloads.py
@@ -1,7 +1,6 @@
 # Run this script to download the necessary NLTK data files
 import nltk
 
-nltk.download("wordnet")
-nltk.download("punkt")
-nltk.download("punkt_tab")
-nltk.download("stopwords")
+for info_or_id in ("wordnet", "punkt", "punkt_tab", "stopwords"):
+    if not nltk.download(info_or_id):
+        raise Exception(f"Unable to download NLTK data file: {info_or_id!r}.")
